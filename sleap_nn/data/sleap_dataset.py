be78313dea0f3fdd2ec2502b9a08774dbe5d61fa
David Samy <davidasamy@gmail.com>
2023-07-06 12:14:29 -0700

    Make SleapDataset general to be used by many pipelines

--- sleap_nn/data/sleap_dataset.py
+++ sleap_nn/data/sleap_dataset.py
@@ -16,23 +16,13 @@ class SleapDataset(Dataset):
     def __init__(
         self,
         labels: sio.Labels,
-        output_stride: int = 2,
-        sigma: int = 2.5,
     ):
         """Initialize SleapDataset.
 
         Args:
             labels: sleap_io.Labels object that contains LabeledFrames
-            output_stride: Relative stride of the generated confidence maps. This is
-                effectively the reciprocal of the output scale, i.e., increase this to
-                generate confidence maps that are smaller than the input images.
-            sigma: Standard deviation of the 2D Gaussian distribution sampled to
-                generate confidence maps. This defines the spread in units of the input
-                image’s grid, i.e., it does not take scaling in previous steps into account.
         """
         self.labels = labels
-        self.sigma = sigma
-        self.output_stride = output_stride
         self.data = []
 
         # Store data in self.data -> format [(LabeledFrame index, Instance index),
@@ -49,7 +39,7 @@ class SleapDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, index: int):
-        """Return a single item."""
+        """Return a single item corresponding to a single instance in a single frame."""
         lf_idx, inst_idx = self.data[index]
         lf = self.labels[lf_idx]
         inst = lf[inst_idx]
