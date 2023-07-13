b95339a5f4c6f6b3d8681b933ef9845e61cf7f9c
David Samy <davidasamy@gmail.com>
2023-07-13 14:41:22 -0700

    Add comments + torchdata dependency

--- sleap_nn/data/providers.py
+++ sleap_nn/data/providers.py
@@ -9,13 +9,21 @@ class LabelsReader(dp.IterDataPipe):
     """Datapipe for reading frames from Labels object."""
 
     def __init__(self, labels: sio.Labels):
+        """Initialize LabelsReader.
+
+        Args:
+            labels: sleap_io.Labels object that contains LabeledFrames that will be
+                accessed through a torchdata DataPipe
+        """
         self.labels = labels
 
     @classmethod
     def from_filename(cls, filename: str):
+        """Create LabelsReader from a .slp filename."""
         labels = sio.load_slp(filename)
         return cls(labels)
 
     def __iter__(self):
+        """Return labeledframe sample."""
         for lf in self.labels:
             yield lf
