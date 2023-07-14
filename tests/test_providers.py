88f8e503892872256c8ad593284dbd9167468a23
David Samy <davidasamy@gmail.com>
2023-07-14 15:01:03 -0700

    patch lightning issue

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -3,14 +3,14 @@ import sleap_io as sio
 import pytest
 
 
-def test_sleap_dataset(minimal_instance):
+def test_providers(minimal_instance):
     """Test sleap dataset
 
     Args:
         minimal_instance: minimal_instance testing fixture
     """
-    l = LabelsReader.from_filename("tests\data\minimal_instance.pkg.slp")
+    l = LabelsReader.from_filename(minimal_instance)
     first_lf = next(iter(l))
     assert len(first_lf) == 2
-    assert first_lf.video.filename == "tests/data/minimal_instance.pkg.slp"
+    assert "minimal_instance" in first_lf.video.filename
     assert first_lf.video.shape == (1, 384, 384, 1)
