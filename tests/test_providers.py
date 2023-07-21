e5799257d6bb9b5f6a024c61029785921e86dd81
David Samy <96505813+davidasamy@users.noreply.github.com>
2023-07-21 16:33:49 -0700

    Add centroid finder block (#7)
    * Add InstanceCentroid class and corresponding functions
    
    index on david/instance-centroids: 8daa796 Add InstanceCentroid class and corresponding functions
    
    remove debug code
    
    * Revise labels provider to yield frame-level examples
    
    * Add anchor preference logic in centroid finder
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Talmo Pereira <talmo@salk.edu>

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -5,12 +5,13 @@ import torch
 
 
 def test_providers(minimal_instance):
-    """Test sleap dataset
+    """Test LabelsReader
 
     Args:
         minimal_instance: minimal_instance testing fixture
     """
     l = LabelsReader.from_filename(minimal_instance)
-    instance, image = next(iter(l))
-    assert image.shape == torch.Size([384, 384, 1])
-    assert instance.shape == torch.Size([2, 2])
+    sample = next(iter(l))
+    instances, image = sample["instances"], sample["image"]
+    assert image.shape == torch.Size([1, 1, 384, 384])
+    assert instances.shape == torch.Size([1, 2, 2, 2])
