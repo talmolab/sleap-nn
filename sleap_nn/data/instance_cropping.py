988d95cc0d047e196ae671e4a0a914282de7262c
David Samy <davidasamy@gmail.com>
2023-07-25 15:51:21 -0700

    Patch cropping issue. Add docstrings.
    Fix implementation

--- sleap_nn/data/instance_cropping.py
+++ sleap_nn/data/instance_cropping.py
@@ -3,7 +3,7 @@ import torchdata.datapipes.iter as dp
 import lightning.pytorch as pl
 from typing import Optional
 import sleap_io as sio
-from kornia.geometry.transoform import crop_and_resize
+from kornia.geometry.transform import crop_and_resize
 import numpy as np
 import torch
 
@@ -11,60 +11,29 @@ import torch
 def make_centered_bboxes(
     centroids: torch.Tensor, box_height: int, box_width: int
 ) -> torch.Tensor:
-    delta = (
-        torch.Tensor(
-            [[-box_height + 1, -box_width + 1, box_height - 1, box_width - 1]],
-            torch.float32,
-        )
-        * 0.5
-    )
-    bboxes = torch.gather(centroids, [1, 0, 1, 0], dim=-1) + delta
-    return bboxes
+    """Create centered bounding boxes around centroid.
 
+    To be used with `kornia.geometry.transform.crop_and_resize`in the following (clockwise)
+    order: top-left, top-right, bottom-right and bottom-left.
+    The coordinates must be in the x, y order.
+    """
+    half_h = box_height / 2
+    half_w = box_width / 2
 
-def normalize_bboxes(
-    bboxes: torch.Tensor, image_height: int, image_width: int
-) -> torch.Tensor:
-    factor = (
-        torch.Tensor([[image_height, image_width, image_height, image_width]]).to(
-            torch.float32
-        )
-        - 1
-    )
+    # Get x and y values from the centroids tensor
+    x = centroids[..., 0]
+    y = centroids[..., 1]
 
-    # Normalize and return.
-    normalized_bboxes = bboxes / factor
-    return normalized_bboxes
+    # Calculate the corner points
+    top_left = torch.stack([x - half_w, y - half_h], dim=-1)
+    top_right = torch.stack([x + half_w, y - half_h], dim=-1)
+    bottom_left = torch.stack([x - half_w, y + half_h], dim=-1)
+    bottom_right = torch.stack([x + half_w, y + half_h], dim=-1)
 
+    # Get bounding box
+    corners = torch.stack([top_left, top_right, bottom_right, bottom_left], dim=-2)
 
-def crop_bboxes(image: torch.Tensor, bboxes: torch.Tensor) -> torch.Tensor:
-
-    indices = [[0, 0], [0, 1]]
-    y1x1 = bboxes.masked_select(indices)
-    indices = [[0, 2], [0, 3]]
-    y2x2 = bboxes.masked_select(indices)
-
-    box_size = torch.round((y2x2 - y1x1) + 1).int()  # (height, width)
-
-    # Normalize bounding boxes.
-    image_height = image.shape[0]
-    image_width = image.shape[1]
-    normalized_bboxes = normalize_bboxes(
-        bboxes, image_height=image_height, image_width=image_width
-    )
-
-    # Crop.
-    crops = crop_and_resize(
-        tensor=torch.unsqueeze(image, 0),
-        boxes=normalized_bboxes,
-
-        size=box_size
-        mode="bilinear"
-    )
-
-    # Cast back to original dtype and return.
-    crops = crops.to(image.dtype)
-    return crops
+    return corners
 
 
 class InstanceCropper(dp.IterDataPipe):
@@ -92,17 +61,40 @@ class InstanceCropper(dp.IterDataPipe):
     def __iter__(self):
         """Add `"centroids"` key to example."""
         for example in self.source_dp:
-            bboxes = make_centered_bboxes(
-                example["centroids"], self.crop_height, self.crop_width
-            )
+            image = example["image"]
+            instances = example["instances"]
+            centroids = example["centroids"]
+            for instance, centroid in zip(instances[0], centroids[0]):
+                print(instance)
+                print(centroid)
+                bboxes = torch.unsqueeze(
+                    make_centered_bboxes(centroid, self.crop_height, self.crop_width), 0
+                )
 
-            instance_images = crop_bboxes(example["image"], bboxes)
+                print(f"make  {bboxes}")
 
-            bboxes_x1y1 = torch.gather(bboxes, [1, 0], dim=1)
-            n_instances = bboxes.shape[0]
-            all_instances = torch.repeat_interleave(
-                example["instances"].unsqueeze(0),
-                n_instances,
-                dim=0,
-            )
-            all_instances = all_instances - bboxes_x1y1.view(n_instances, 1, 1, 2)
+                # Crop.
+                box_size = (self.crop_width, self.crop_height)
+                print(f" boxes {bboxes.shape}")
+                print(f" image {image.shape}")
+                print(f" box_size {box_size}")
+
+                instance_images = crop_and_resize(
+                    image,
+                    boxes=bboxes,
+                    size=box_size,
+                )
+
+                point = bboxes[0][0]
+                center_instances = torch.sub(instance, point)
+
+                example = {
+                    "instance_image": instance_images,
+                    "image": image,
+                    "bbox": bboxes,
+                    "instances": instances,
+                    "centered_instances": center_instances,
+                    "centroids": centroids,
+                }
+                # TODO return instance-wise examples
+                yield example
