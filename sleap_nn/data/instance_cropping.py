1088e7fea5a6668f25fafc64b1ae334e7b070f43
alckasoc <tuvincent0106@gmail.com>
2023-08-03 07:55:25 -0700

    added make_centered_bboxes & normalize_bboxes

--- sleap_nn/data/instance_cropping.py
+++ sleap_nn/data/instance_cropping.py
@@ -6,6 +6,39 @@ from kornia.geometry.transform import crop_and_resize
 import numpy as np
 import torch
 
+def normalize_bboxes(
+    bboxes: torch.Tensor, image_height: int, image_width: int
+) -> torch.Tensor:
+    """Normalize bounding box coordinates to the range [0, 1].
+
+    This is useful for transforming points for PyTorch operations that require
+    normalized image coordinates.
+
+    Args:
+        bboxes: Tensor of shape (n_bboxes, 4) and dtype torch.float32, where the last axis
+            corresponds to (y1, x1, y2, x2) coordinates of the bounding boxes.
+        image_height: Scalar integer indicating the height of the image.
+        image_width: Scalar integer indicating the width of the image.
+
+    Returns:
+        Tensor of the normalized points of the same shape as `bboxes`.
+
+        The normalization applied to each point is `x / (image_width - 1)` and
+        `y / (image_width - 1)`.
+
+    See also: unnormalize_bboxes
+    """
+    # Compute normalizing factor of shape (1, 4).
+    factor = (
+        torch.tensor(
+            [[image_height, image_width, image_height, image_width]], dtype=torch.float32
+        ) 
+        - 1
+    )
+
+    # Normalize and return.
+    normalized_bboxes = bboxes / factor
+    return normalized_bboxes
 
 def make_centered_bboxes(
     centroids: torch.Tensor, box_height: int, box_width: int
---
+++ sleap_nn/inference/peak_finding.py
@@ -0,0 +1,5 @@
+import torch
+import numpy as np
+from typing import Tuple, Optional
+from sleap_nn.data.instance_cropping import make_centered_bboxes, normalize_bboxes
+
