691af4548f37fbc24d6ed00d8e21e176b317bee1
alckasoc <tuvincent0106@gmail.com>
2023-08-18 18:30:34 -0700

    added tests for simple upsampling block

--- tests/architectures/test_encoder_decoder.py
+++ tests/architectures/test_encoder_decoder.py
@@ -1,7 +1,10 @@
 import torch
 
 import sleap_nn
-from sleap_nn.architectures.encoder_decoder import SimpleConvBlock
+from sleap_nn.architectures.encoder_decoder import (
+    SimpleConvBlock,
+    SimpleUpsamplingBlock,
+)
 
 
 def test_simple_conv_block():
@@ -25,3 +28,79 @@ def test_simple_conv_block():
     block.blocks[4].__class__ == torch.nn.modules.batchnorm.BatchNorm2d
     block.blocks[5].__class__ == torch.nn.modules.activation.ReLU
     block.blocks[6].__class__ == sleap_nn.architectures.common.MaxPool2dWithSamePadding
+
+
+def test_simple_upsampling_block():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    block = SimpleUpsamplingBlock(
+        x_in_shape=10,
+        current_stride=1,
+        upsampling_stride=2,
+        interp_method="bilinear",
+        refine_convs=2,
+        refine_convs_filters=64,
+        refine_convs_kernel_size=3,
+        refine_convs_use_bias=True,
+        refine_convs_batch_norm=True,
+        refine_convs_batch_norm_before_activation=True,
+        refine_convs_activation="relu",
+    )
+
+    block = block.to(device)
+    _ = block.eval()
+
+    x = torch.rand(5, 5, 100, 100).to(device)
+    feature = torch.rand(5, 5, 200, 200).to(device)
+
+    z = block(x, feature=feature)
+
+    assert z.shape == (5, 64, 200, 200)
+
+    block = SimpleUpsamplingBlock(
+        x_in_shape=10,
+        current_stride=1,
+        upsampling_stride=2,
+        interp_method="bilinear",
+        refine_convs=2,
+        refine_convs_filters=64,
+        refine_convs_kernel_size=3,
+        refine_convs_use_bias=True,
+        refine_convs_batch_norm=True,
+        refine_convs_batch_norm_before_activation=True,
+        refine_convs_activation="relu",
+    )
+
+    block = block.to(device)
+    _ = block.eval()
+
+    x = torch.rand(5, 5, 100, 100).to(device)
+    feature = torch.rand(5, 5, 200, 200).to(device)
+
+    z = block(x, feature=feature)
+
+    assert z.shape == (5, 64, 200, 200)
+
+    block = SimpleUpsamplingBlock(
+        x_in_shape=10,
+        current_stride=1,
+        upsampling_stride=2,
+        interp_method="bilinear",
+        refine_convs=2,
+        refine_convs_filters=64,
+        refine_convs_kernel_size=3,
+        refine_convs_use_bias=True,
+        refine_convs_batch_norm=True,
+        refine_convs_batch_norm_before_activation=False,
+        refine_convs_activation="relu",
+    )
+
+    block = block.to(device)
+    _ = block.eval()
+
+    x = torch.rand(5, 5, 100, 100).to(device)
+    feature = torch.rand(5, 5, 200, 200).to(device)
+
+    z = block(x, feature=feature)
+
+    assert z.shape == (5, 64, 200, 200)
