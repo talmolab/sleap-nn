029a5454d4ef03be98e63e5acc1e668efd4a745a
alckasoc <tuvincent0106@gmail.com>
2023-08-30 16:05:40 -0700

    created data/pipelines.py

---
+++ sleap_nn/data/pipelines.py
@@ -0,0 +1,5 @@
+"""This module defines high level pipeline configurations from providers/transformers.
+
+This allows for convenient ways to configure individual variants of common pipelines, as
+well as to define training vs inference versions based on the same configurations.
+"""
