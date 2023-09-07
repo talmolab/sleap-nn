b1ef93cecfe2a88426e53a95a9a449fb8747c378
alckasoc <tuvincent0106@gmail.com>
2023-09-06 17:08:14 -0700

    updated test_pipelines

--- tests/data/test_pipelines.py
+++ tests/data/test_pipelines.py
@@ -66,3 +66,46 @@ def test_topdownconfmapspipeline(minimal_instance):
     assert len(sample) == 2
     assert sample[0].shape == (1, 160, 160)
     assert sample[1].shape == (2, 80, 80)
+
+    base_topdown_data_config = OmegaConf.create(
+        {
+            "preprocessing": {
+                "crop_hw": (160, 160),
+                "conf_map_gen": {"sigma": 1.5, "output_stride": 2},
+            },
+            "augmentation_config": {
+                "random_crop": {"random_crop_p": 1.0, "random_crop_hw": (160, 160)},
+                "use_augmentations": True,
+                "augmentations": {
+                    "rotation": 15.0,
+                    "scale": 0.05,
+                    "translate": (0.02, 0.02),
+                    "affine_p": 0.5,
+                    "uniform_noise": (0.0, 0.04),
+                    "uniform_noise_p": 0.5,
+                    "gaussian_noise_mean": 0.02,
+                    "gaussian_noise_std": 0.004,
+                    "gaussian_noise_p": 0.5,
+                    "contrast": (0.5, 2.0),
+                    "contrast_p": 0.5,
+                    "brightness": 0.0,
+                    "brightness_p": 0.5,
+                    "erase_scale": (0.0001, 0.01),
+                    "erase_ratio": (1, 1),
+                    "erase_p": 0.5,
+                    "mixup_lambda": None,
+                    "mixup_p": 0.5,
+                },
+            },
+        }
+    )
+
+    pipeline = TopdownConfmapsPipeline(data_config=base_topdown_data_config)
+    datapipe = pipeline.make_training_pipeline(
+        data_provider=LabelsReader, filename=minimal_instance
+    )
+
+    sample = next(iter(datapipe))
+    assert len(sample) == 2
+    assert sample[0].shape == (1, 160, 160)
+    assert sample[1].shape == (2, 80, 80)
