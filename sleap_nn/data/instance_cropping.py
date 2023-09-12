e3b28da3b52eaaf34e4d5af2a6c7c10509a6ecbc
alckasoc <tuvincent0106@gmail.com>
2023-09-12 10:56:05 -0700

    fixed small squeeze issue

--- sleap_nn/data/instance_cropping.py
+++ sleap_nn/data/instance_cropping.py
@@ -93,7 +93,9 @@ class InstanceCropper(IterDataPipe):
                 center_instance = instance - point
 
                 instance_example = {
-                    "instance_image": instance_image.squeeze(),  # (B=1, channels, crop_height, crop_width)
+                    "instance_image": instance_image.squeeze(
+                        0
+                    ),  # (B=1, channels, crop_height, crop_width)
                     "instance_bbox": instance_bbox,  # (B, 4, 2)
                     "instance": center_instance,  # (num_nodes, 2)
                 }
