0dff84fd4f0069c41a7414336a5980121f95ac7a
Vincent Tu <58043265+alckasoc@users.noreply.github.com>
2023-10-19 09:45:24 -0700

    Implement base Model and Head classes (#17)
    * added make_centered_bboxes & normalize_bboxes
    
    * added make_centered_bboxes & normalize_bboxes
    
    * created test_instance_cropping.py
    
    * added test normalize bboxes; added find_global_peaks_rough
    
    * black formatted
    
    * black formatted peak_finding
    
    * added make_grid_vectors, normalize_bboxes, integral_regression, added docstring to make_centered_bboxes, fixed find_global_peaks_rough; added crop_bboxes
    
    * finished find_global_peaks with integral regression over centroid crops!
    
    * reformatted with pydocstyle & black
    
    * moved make_grid_vectors to data/utils
    
    * removed normalize_bboxes
    
    * added tests docstrings
    
    * sorted imports with isort
    
    * remove unused imports
    
    * updated test cases for instance cropping
    
    * added minimal_cms.pt fixture + unit tests
    
    * added minimal_bboxes fixture; added unit tests for crop_bboxes & integral_regression
    
    * added find_global_peaks unit tests
    
    * finished find_local_peaks_rough!
    
    * finished find_local_peaks!
    
    * added unit tests for find_local_peaks and find_local_peaks_rough
    
    * updated test cases
    
    * added more test cases for find_local_peaks
    
    * updated test cases
    
    * added architectures folder
    
    * added maxpool2d same padding, get_act_fn; added simpleconvblock, simpleupsamplingblock, encoder, decoder; added unet
    
    * added test_unet_reference
    
    * black formatted common.py & test_unet.py
    
    * deleted tmp nb
    
    * _calc_same_pad returns int
    
    * fixed test case
    
    * added simpleconvblock tests
    
    * added tests
    
    * added tests for simple upsampling block
    
    * updated test_unet
    
    * removed unnecessary variables
    
    * updated augmentation random erase default values
    
    * created data/pipelines.py
    
    * added base config in config/data; temporary till config system settled
    
    * updated variable defaults to 0 and edited variable names in augmentation
    
    * updated parameter names in data/instance_cropping
    
    * added data/pipelines topdown pipeline make_base_pipeline
    
    * added test_pipelines
    
    * removed configs
    
    * updated augmentation class
    
    * modified test
    
    * removed cuda cache
    
    * added Model builder class and heads
    
    * added type hinting for init
    
    * black reformatted heads.py
    
    * updated model.py
    
    * updated test_model.py
    
    * updated test_model.py
    
    * updated pipelines docstring
    
    * added from_config for Model
    
    * added more act fn to get_act_fn
    
    * black reformatted & updated model.py & test_model.py
    
    * updated config, typehints, black formatted & added doc strings
    
    * added test_heads.py
    
    * updated module docstring
    
    * updated Model docstring
    
    * added coderabbit suggestions
    
    * black reformat
    
    * added 2 helper methods for getting backbone/head; separated common and utils in architectures
    
    * removed comments
    
    * updated test_get_act_fn
    
    * added multi-head feature to Model
    
    * black reformatted model.py
    
    * added all test cases for heads.py
    
    * reformatted test_heads.py
    
    * updated L44 in confidence_maps.py
    
    * added output channels to unet
    
    * resolved merge conflicts + small bugs
    
    * black reformatted
    
    * added coderabbit suggestions
    
    * not sure how intermediate features + multi head would work
    
    * Separate Augmentations into Intensity and Geometric (#18)
    
    * initial commit
    
    * separated intensity and geometric augmentations
    
    * test
    
    * pseudo code in model.py
    
    * small fix
    
    * name property in heads.py
    
    * name of head docstring added
    
    * added ruff cache to gitignore; added head selection in Model class
    
    * updated return value for decoder
    
    * small change to model.py
    
    * made model.py forward more efficient
    
    * small comments updated in instance_cropping for clarity
    
    * updated output structure of unet to dict; updated model.py attribute head
    
    * fixed minor changes
    
    * fixed minor changes
    
    * updated ruff output format
    
    * added anchor_ind to topdown pipeline config

--- tests/data/test_pipelines.py
+++ tests/data/test_pipelines.py
@@ -10,7 +10,7 @@ from sleap_nn.data.pipelines import TopdownConfmapsPipeline
 from sleap_nn.data.providers import LabelsReader
 
 
-def test_sleap_dataset(minimal_instance):
+def test_key_filter(minimal_instance):
     datapipe = LabelsReader.from_filename(filename=minimal_instance)
     datapipe = Normalizer(datapipe)
     datapipe = InstanceCentroidFinder(datapipe)
@@ -42,6 +42,7 @@ def test_topdownconfmapspipeline(minimal_instance):
         {
             "general": {"keep_keys": ["instance_image", "confidence_maps"]},
             "preprocessing": {
+                "anchor_ind": None,
                 "crop_hw": (160, 160),
                 "conf_map_gen": {"sigma": 1.5, "output_stride": 2},
             },
@@ -49,24 +50,28 @@ def test_topdownconfmapspipeline(minimal_instance):
                 "random_crop": {"random_crop_p": 1.0, "random_crop_hw": (160, 160)},
                 "use_augmentations": False,
                 "augmentations": {
-                    "rotation": 15.0,
-                    "scale": 0.05,
-                    "translate": (0.02, 0.02),
-                    "affine_p": 0.5,
-                    "uniform_noise": (0.0, 0.04),
-                    "uniform_noise_p": 0.5,
-                    "gaussian_noise_mean": 0.02,
-                    "gaussian_noise_std": 0.004,
-                    "gaussian_noise_p": 0.5,
-                    "contrast": (0.5, 2.0),
-                    "contrast_p": 0.5,
-                    "brightness": 0.0,
-                    "brightness_p": 0.5,
-                    "erase_scale": (0.0001, 0.01),
-                    "erase_ratio": (1, 1),
-                    "erase_p": 0.5,
-                    "mixup_lambda": None,
-                    "mixup_p": 0.5,
+                    "intensity": {
+                        "uniform_noise": (0.0, 0.04),
+                        "uniform_noise_p": 0.5,
+                        "gaussian_noise_mean": 0.02,
+                        "gaussian_noise_std": 0.004,
+                        "gaussian_noise_p": 0.5,
+                        "contrast": (0.5, 2.0),
+                        "contrast_p": 0.5,
+                        "brightness": 0.0,
+                        "brightness_p": 0.5,
+                    },
+                    "geometric": {
+                        "rotation": 15.0,
+                        "scale": 0.05,
+                        "translate": (0.02, 0.02),
+                        "affine_p": 0.5,
+                        "erase_scale": (0.0001, 0.01),
+                        "erase_ratio": (1, 1),
+                        "erase_p": 0.5,
+                        "mixup_lambda": None,
+                        "mixup_p": 0.5,
+                    },
                 },
             },
         }
@@ -91,6 +96,7 @@ def test_topdownconfmapspipeline(minimal_instance):
         {
             "general": {"keep_keys": None},
             "preprocessing": {
+                "anchor_ind": None,
                 "crop_hw": (160, 160),
                 "conf_map_gen": {"sigma": 1.5, "output_stride": 2},
             },
@@ -98,24 +104,28 @@ def test_topdownconfmapspipeline(minimal_instance):
                 "random_crop": {"random_crop_p": 1.0, "random_crop_hw": (160, 160)},
                 "use_augmentations": True,
                 "augmentations": {
-                    "rotation": 15.0,
-                    "scale": 0.05,
-                    "translate": (0.02, 0.02),
-                    "affine_p": 0.5,
-                    "uniform_noise": (0.0, 0.04),
-                    "uniform_noise_p": 0.5,
-                    "gaussian_noise_mean": 0.02,
-                    "gaussian_noise_std": 0.004,
-                    "gaussian_noise_p": 0.5,
-                    "contrast": (0.5, 2.0),
-                    "contrast_p": 0.5,
-                    "brightness": 0.0,
-                    "brightness_p": 0.5,
-                    "erase_scale": (0.0001, 0.01),
-                    "erase_ratio": (1, 1),
-                    "erase_p": 0.5,
-                    "mixup_lambda": None,
-                    "mixup_p": 0.5,
+                    "intensity": {
+                        "uniform_noise": (0.0, 0.04),
+                        "uniform_noise_p": 0.5,
+                        "gaussian_noise_mean": 0.02,
+                        "gaussian_noise_std": 0.004,
+                        "gaussian_noise_p": 0.5,
+                        "contrast": (0.5, 2.0),
+                        "contrast_p": 0.5,
+                        "brightness": 0.0,
+                        "brightness_p": 0.5,
+                    },
+                    "geometric": {
+                        "rotation": 15.0,
+                        "scale": 0.05,
+                        "translate": (0.02, 0.02),
+                        "affine_p": 0.5,
+                        "erase_scale": (0.0001, 0.01),
+                        "erase_ratio": (1, 1),
+                        "erase_p": 0.5,
+                        "mixup_lambda": None,
+                        "mixup_p": 0.5,
+                    },
                 },
             },
         }
