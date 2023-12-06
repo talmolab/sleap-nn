6dcd44807b8b4f4f11835f23a92d13ce1071a7a8
gitttt-1234 <64513125+gitttt-1234@users.noreply.github.com>
2023-12-05 17:49:48 -0800

    Add test function

--- sleap_nn/model_trainer.py
+++ sleap_nn/model_trainer.py
@@ -5,6 +5,7 @@ import sleap_io as sio
 from torch.utils.data import DataLoader
 from typing import Text
 import lightning.pytorch as pl
+import pandas as pd
 from omegaconf import OmegaConf
 import lightning as L
 from sleap_nn.data.providers import LabelsReader
@@ -17,134 +18,183 @@ from sleap_nn.architectures.model import Model
 from lightning.pytorch.callbacks import ModelCheckpoint
 
 
-class ModelTrainer():
+class ModelTrainer:
     """Train sleap-nn model using PyTorch Lightning.
 
     This class is used to train a sleap-nn model and save the model checkpoints/ logs with options to logging
-    with wandb and csvlogger. 
+    with wandb and csvlogger.
 
     Args:
-        data_config: OmegaConf dictionary which has data loading pre-processing configs
-                    to be passed to `TopdownConfmapsPipeline` class.
-        model_config: OmegaConf dictionary consisting of backbone and head configs to be passed to `Model` class.
-        trainer_config : OemgaConf dictionary having all trainer configs like accelerator, optimiser params.
-    
+        config: OmegaConf dictionary which has the following:
+                (i) data_config: data loading pre-processing configs to be passed to `TopdownConfmapsPipeline` class.
+                (ii) model_config: backbone and head configs to be passed to `Model` class.
+                (iii) trainer_cong: trainer configs like accelerator, optimiser params.
+
     """
-    def __init__(self, data_config: OmegaConf, model_config: OmegaConf, trainer_config: OmegaConf):
+
+    def __init__(self, config: OmegaConf):
         """Initialise the class with configs and set the seed and device as class attributes."""
-        self.data_config = data_config
-        self.model_config = model_config
-        self.trainer_config = trainer_config
-        self.m_device = self.trainer_config.device
-        self.seed = self.trainer_config.seed
+        self.config = config
+
+        self.m_device = self.config.trainer_config.device
+        self.seed = self.config.trainer_config.seed
         # set seed
         torch.manual_seed(self.seed)
-
 
     def _create_data_loaders(self):
         """Creates a DataLoader for train, validation and test sets using the data_config."""
 
-        self.provider = self.data_config.provider
+        self.provider = self.config.data_config.provider
         if self.provider == "LabelsReader":
             self.provider = LabelsReader
-        
+
         # create pipelines
-        train_pipeline = TopdownConfmapsPipeline(data_config=self.data_config.train)
+        train_pipeline = TopdownConfmapsPipeline(
+            data_config=self.config.data_config.train
+        )
         train_datapipe = train_pipeline.make_training_pipeline(
-            data_provider=self.provider, filename=self.data_config.train.labels_path
+            data_provider=self.provider,
+            filename=self.config.data_config.train.labels_path,
         )
 
-        val_pipeline = TopdownConfmapsPipeline(data_config=self.data_config.valid)
+        val_pipeline = TopdownConfmapsPipeline(
+            data_config=self.config.data_config.valid
+        )
         val_datapipe = val_pipeline.make_training_pipeline(
-            data_provider=LabelsReader, filename=self.data_config.valid.labels_path
+            data_provider=LabelsReader,
+            filename=self.config.data_config.valid.labels_path,
         )
 
-        use_test_data = "test" in self.data_config.keys()
+        use_test_data = "test" in self.config.data_config.keys()
         if use_test_data:
-            test_pipeline = TopdownConfmapsPipeline(data_config=self.data_config.test)
-            test_datapipe = test_pipeline.make_training_pipeline(
-                data_provider=LabelsReader, filename=self.data_config.test.labels_path
+            test_pipeline = TopdownConfmapsPipeline(
+                data_config=self.config.data_config.test
             )
+            test_datapipe = test_pipeline.make_training_pipeline(
+                data_provider=LabelsReader,
+                filename=self.config.data_config.test.labels_path,
+            )
+            test_datapipe = test_datapipe.sharding_filter()
 
         # to remove duplicates when multiprocessing is used
         train_datapipe = train_datapipe.sharding_filter()
         val_datapipe = val_datapipe.sharding_filter()
-        test_datapipe = test_datapipe.sharding_filter()
 
-        
-        num_cores = self.trainer_config.num_cores
+        num_cores = self.config.trainer_config.num_cores
         # create torch Data Loaders
-        self.train_data_loader = DataLoader(train_datapipe, batch_size=self.trainer_config.train_batch_size, shuffle=True, num_workers=num_cores, pin_memory=True, drop_last=True, prefetch_factor=2)
-        self.val_data_loader = DataLoader( val_datapipe, batch_size=self.trainer_config.val_batch_size, shuffle=False, num_workers=num_cores, pin_memory=True, drop_last=True, prefetch_factor=2)
+        self.train_data_loader = DataLoader(
+            train_datapipe,
+            batch_size=self.config.trainer_config.train_batch_size,
+            shuffle=True,
+            num_workers=num_cores,
+            pin_memory=True,
+            drop_last=True,
+            prefetch_factor=2,
+        )
+        self.val_data_loader = DataLoader(
+            val_datapipe,
+            batch_size=self.config.trainer_config.val_batch_size,
+            shuffle=False,
+            num_workers=num_cores,
+            pin_memory=True,
+            drop_last=True,
+            prefetch_factor=2,
+        )
+        self.test_data_loader = None
         if use_test_data:
-            self.test_data_loader = DataLoader(test_datapipe, batch_size=self.trainer_config.test_batch_size, shuffle=False, num_workers=num_cores, pin_memory=True, drop_last=True, prefetch_factor=2)
+            self.test_data_loader = DataLoader(
+                test_datapipe,
+                batch_size=self.config.trainer_config.test_batch_size,
+                shuffle=False,
+                num_workers=num_cores,
+                pin_memory=True,
+                drop_last=True,
+                prefetch_factor=2,
+            )
 
     def _set_wandb(self):
         wandb.login()
-        self.wandb_logger = WandbLogger(project=self.trainer_config.wandb.project, name=self.trainer_config.wandb.entity_name)
+        self.wandb_logger = WandbLogger(
+            project=self.config.trainer_config.wandb.project,
+            name=self.config.trainer_config.wandb.entity_name,
+        )
 
     def train(self):
         """Function to initiate the training by calling the fit method of Trainer"""
         self._create_data_loaders()
-        dir_path = self.trainer_config.save_ckpt_path
+        dir_path = self.config.trainer_config.save_ckpt_path
         # create checkpoint callback
         checkpoint_callback = ModelCheckpoint(
-                                    save_top_k=1,
-                                    save_last=True,
-                                    every_n_epochs=0,
-                                    monitor="val_loss",
-                                    mode="min",
-                                    dirpath=dir_path,
-                                    filename="{epoch:02d}-{val_loss:.2f}",
-                                )
+            save_top_k=1,
+            save_last=True,
+            every_n_epochs=0,
+            monitor="val_loss",
+            mode="min",
+            dirpath=dir_path,
+            filename="{epoch:02d}-{val_loss:.2f}",
+        )
         enable_checkpoint = True
         callbacks = [checkpoint_callback]
         # default logger to create csv with metrics values over the epochs
         csv_logger = CSVLogger(dir_path)
-        logger = csv_logger
-        if self.trainer_config.use_wandb:
+        self.logger = [csv_logger]
+        if self.config.trainer_config.use_wandb:
             self._set_wandb()
-            logger = [self.wandb_logger, csv_logger]
-        if not self.trainer_config.save_ckpt:
+            self.logger = [self.wandb_logger, csv_logger]
+        if not self.config.trainer_config.save_ckpt:
             enable_checkpoint = False
             callbacks = []
-        model =  TopDownCenteredInstanceModel(self.model_config, self.trainer_config, self.data_config)
-        trainer = L.Trainer(callbacks=callbacks, logger=logger, enable_checkpointing=enable_checkpoint, devices=self.trainer_config.devices, max_epochs=self.trainer_config.max_epochs,accelerator=self.trainer_config.trainer_accelerator, enable_progress_bar=self.trainer_config.enable_progress_bar)
-        
+        model = TopDownCenteredInstanceModel(self.config)
+        trainer = L.Trainer(
+            callbacks=callbacks,
+            logger=self.logger,
+            enable_checkpointing=enable_checkpoint,
+            devices=self.config.trainer_config.devices,
+            max_epochs=self.config.trainer_config.max_epochs,
+            accelerator=self.config.trainer_config.trainer_accelerator,
+            enable_progress_bar=self.config.trainer_config.enable_progress_bar,
+        )
+
         trainer.fit(model, self.train_data_loader, self.val_data_loader)
         # save the configs as yaml in the checkpoint dir
-        OmegaConf.save(config=self.trainer_config, f=dir_path+"trainer_config.yaml")
-        OmegaConf.save(config=self.data_config, f=dir_path+"data_config.yaml")
-        OmegaConf.save(config=self.model_config, f=dir_path+"model_config.yaml")
+        OmegaConf.save(config=self.config, f=dir_path + "config.yaml")
+
         wandb.finish()
 
 
 def xavier_init_weights(x):
-    if(isinstance(x,nn.Conv2d) or isinstance(x,nn.Linear)):
+    if isinstance(x, nn.Conv2d) or isinstance(x, nn.Linear):
         nn.init.xavier_uniform_(x.weight)
-        nn.init.constant_(x.bias,0)
+        nn.init.constant_(x.bias, 0)
+
 
 class TopDownCenteredInstanceModel(L.LightningModule):
     """PyTorch Lightning Module.
 
-    This class is a sub-class of Lightning module to configure the training and validation steps. 
+    This class is a sub-class of Lightning module to configure the training and validation steps.
 
     Args:
-        data_config: OmegaConf dictionary which has data loading pre-processing configs
-                    to be passed to `TopdownConfmapsPipeline` class.
-        model_config: OmegaConf dictionary consisting of backbone and head configs to be passed to `Model` class.
-        trainer_config : OemgaConf dictionary having all trainer configs like accelerator, optimiser params.
-    
+        config: OmegaConf dictionary which has the following:
+                (i) data_config: data loading pre-processing configs to be passed to `TopdownConfmapsPipeline` class.
+                (ii) model_config: backbone and head configs to be passed to `Model` class.
+                (iii) trainer_cong: trainer configs like accelerator, optimiser params.
+
     """
-    def __init__(self, model_config: OmegaConf, trainer_config: OmegaConf, data_config:OmegaConf):
+
+    def __init__(self, config: OmegaConf):
+        """Initialise the configs and the model."""
         super().__init__()
-        self.model_config = model_config
-        self.trainer_config = trainer_config
-        self.data_config = data_config
+        self.config = config
+        self.model_config = self.config.model_config
+        self.trainer_config = self.config.trainer_config
+        self.data_config = self.config.data_config
         self.m_device = self.trainer_config.device
         self.model = Model(
-                    backbone_config=self.model_config.backbone_config, head_configs=[self.model_config.head_configs]
-                ).to(self.m_device)
+            backbone_config=self.model_config.backbone_config,
+            head_configs=[self.model_config.head_configs],
+        ).to(self.m_device)
+
+        # Initializing the model weights
         if self.model_config.init_weights == "xavier":
             self.model.apply(xavier_init_weights)
         self.seed = self.trainer_config.seed
@@ -153,29 +203,24 @@ class TopDownCenteredInstanceModel(L.LightningModule):
         self.learning_rate = {}
         torch.manual_seed(self.seed)
 
+    @property
+    def device(self):
+        return next(self.model.parameters()).device
+
     def forward(self, inputs):
+        """Forward pass of the model."""
         imgs = inputs["instance_image"].to(self.m_device)
         return self.model(imgs)["CenteredInstanceConfmapsHead"]
-    
+
     def on_save_checkpoint(self, checkpoint):
+        """Configure checkpoint to save parameters."""
         labels_gt = sio.load_slp(self.data_config.train.labels_path)
+        # save the skeletons and seed to the checkpoint file
         checkpoint["skeleton"] = labels_gt.skeletons
         checkpoint["seed"] = self.seed
-        
-        final_df = {}
-        for ind, (train_l, val_l, lr) in enumerate(zip(self.training_loss.values(), self.val_loss.values(), self.learning_rate.values())):
-            final_df[ind] = (train_l, val_l, lr)
-        final_df = pd.DataFrame.from_dict(final_df, orient='index', columns=["training_loss", "valid_loss", "learning_rate"])
-        print(final_df)
-        final_df.to_csv(self.trainer_config.save_ckpt_path+"training_metrics.csv", index=False)
-        # df = pd.DataFrame(self.training_loss)
-        # df1 = pd.DataFrame(self.val_loss)
-        # df2 = pd.DataFrame(self.learning_rate)
-        # df_f = pd.concat([df, df1, df2], axis=1)
-        # df_f.to_csv(self.trainer_config.save_ckpt_path+"training_metrics.csv", index=False)
 
     def training_step(self, batch, batch_idx):
-                
+        """Training step."""
         X, y = batch["instance_image"], batch["confidence_maps"]
 
         X = X.to(self.m_device)
@@ -183,41 +228,61 @@ class TopDownCenteredInstanceModel(L.LightningModule):
 
         y_preds = self.model(X)["CenteredInstanceConfmapsHead"]
         loss = nn.MSELoss()(y_preds, y)
-        self.log("train_loss", loss, prog_bar=True, on_step=False, on_epoch=True, logger=True)
+        self.log(
+            "train_loss", loss, prog_bar=True, on_step=False, on_epoch=True, logger=True
+        )
         self.training_loss[self.current_epoch] = loss.detach().cpu().numpy()
         return loss
-        
-    def validation_step(self, batch, batch_idx):
 
-        X, y = batch["instance_image"].to(self.m_device), batch["confidence_maps"].to(self.m_device)
-        
+    def validation_step(self, batch, batch_idx):
+        """Validation step."""
+        X, y = batch["instance_image"].to(self.m_device), batch["confidence_maps"].to(
+            self.m_device
+        )
+
         y_preds = self.model(X)["CenteredInstanceConfmapsHead"]
         val_loss = nn.MSELoss()(y_preds, y)
-        lr = self.optimizers().optimizer.param_groups[0]['lr']
-        self.log("learning_rate", lr, prog_bar=True, on_step=False, on_epoch=True, logger=True)
-        self.log("val_loss", val_loss, prog_bar=True, on_step=False, on_epoch=True, logger=True)
+        lr = self.optimizers().optimizer.param_groups[0]["lr"]
+        self.log(
+            "learning_rate",
+            lr,
+            prog_bar=True,
+            on_step=False,
+            on_epoch=True,
+            logger=True,
+        )
+        self.log(
+            "val_loss",
+            val_loss,
+            prog_bar=True,
+            on_step=False,
+            on_epoch=True,
+            logger=True,
+        )
         self.val_loss[self.current_epoch] = val_loss.detach().cpu().numpy()
         self.learning_rate[self.current_epoch] = lr
-    
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.trainer_config.optimizer.learning_rate, amsgrad=self.trainer_config.optimizer.use_amsgrad)
+        """Configure optimiser and learning rate scheduler."""
+        optimizer = torch.optim.Adam(
+            self.parameters(),
+            lr=self.trainer_config.optimizer.learning_rate,
+            amsgrad=self.trainer_config.optimizer.use_amsgrad,
+        )
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-                        optimizer,
-                        "min",
-                        threshold=self.trainer_config.optimizer.lr_scheduler_threshold,
-                        threshold_mode="abs",
-                        cooldown=self.trainer_config.optimizer.lr_scheduler_cooldown,
-                        patience=self.trainer_config.optimizer.lr_scheduler_patience,
-                        factor=self.trainer_config.optimizer.lr_reduction_factor,
-                        min_lr=self.trainer_config.optimizer.min_lr,
-                    )
+            optimizer,
+            "min",
+            threshold=self.trainer_config.optimizer.lr_scheduler_threshold,
+            threshold_mode="abs",
+            cooldown=self.trainer_config.optimizer.lr_scheduler_cooldown,
+            patience=self.trainer_config.optimizer.lr_scheduler_patience,
+            factor=self.trainer_config.optimizer.lr_reduction_factor,
+            min_lr=self.trainer_config.optimizer.min_lr,
+        )
         return {
-        "optimizer": optimizer,
-        "lr_scheduler": {
-            "scheduler": scheduler,
-            "monitor": 'val_loss',            
-        },
-    }
-
-
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "monitor": "val_loss",
+            },
+        }
---
+++ tests/test_model_trainer.py
@@ -0,0 +1,176 @@
+import torch
+import sleap_io as sio
+from torch.utils.data import DataLoader
+from typing import Text
+import lightning.pytorch as pl
+from omegaconf import OmegaConf
+import lightning as L
+from sleap_nn.data.providers import LabelsReader
+from sleap_nn.data.pipelines import TopdownConfmapsPipeline
+import wandb
+import os
+from torch import nn
+import pandas as pd
+from sleap_nn.architectures.model import Model
+from sleap_nn.model_trainer import ModelTrainer, TopDownCenteredInstanceModel
+from torch.nn.functional import mse_loss
+
+
+def test_create_data_loader(config):
+    model_trainer = ModelTrainer(config)
+    model_trainer._create_data_loaders()
+    assert isinstance(
+        model_trainer.train_data_loader, torch.utils.data.dataloader.DataLoader
+    )
+    assert isinstance(
+        model_trainer.val_data_loader, torch.utils.data.dataloader.DataLoader
+    )
+    assert model_trainer.test_data_loader is None
+    assert len(list(iter(model_trainer.train_data_loader))) == 2
+    assert len(list(iter(model_trainer.val_data_loader))) == 2
+
+    config_test = OmegaConf.create(
+        {
+            "labels_path": "minimal_instance.pkg.slp",
+            "general": {
+                "keep_keys": [
+                    "instance_image",
+                    "confidence_maps",
+                    "instance",
+                    "video_idx",
+                    "frame_idx",
+                    "instance_bbox",
+                ]
+            },
+            "preprocessing": {
+                "anchor_ind": 0,
+                "crop_hw": (160, 160),
+                "conf_map_gen": {"sigma": 1.5, "output_stride": 2},
+            },
+            "augmentation_config": {
+                "random_crop": {"random_crop_p": 0, "random_crop_hw": (160, 160)},
+                "use_augmentations": False,
+                "augmentations": {
+                    "intensity": {
+                        "uniform_noise": (0.0, 0.04),
+                        "uniform_noise_p": 0,
+                        "gaussian_noise_mean": 0.02,
+                        "gaussian_noise_std": 0.004,
+                        "gaussian_noise_p": 0,
+                        "contrast": (0.5, 2.0),
+                        "contrast_p": 0,
+                        "brightness": 0.0,
+                        "brightness_p": 0,
+                    },
+                    "geometric": {
+                        "rotation": 180.0,
+                        "scale": 0,
+                        "translate": (0, 0),
+                        "affine_p": 0.5,
+                        "erase_scale": (0.0001, 0.01),
+                        "erase_ratio": (1, 1),
+                        "erase_p": 0,
+                        "mixup_lambda": None,
+                        "mixup_p": 0,
+                    },
+                },
+            },
+        },
+    )
+
+    OmegaConf.update(config, "data_config.test", config_test)
+    print(config.data_config.test)
+    model_trainer = ModelTrainer(config)
+    model_trainer._create_data_loaders()
+    assert isinstance(
+        model_trainer.test_data_loader, torch.utils.data.dataloader.DataLoader
+    )
+    assert len(list(iter(model_trainer.test_data_loader))) == 2
+
+
+def test_trainer(config):
+
+    model_trainer = ModelTrainer(config)
+    model_trainer.train()
+    assert all(
+        [
+            not isinstance(i, L.pytorch.loggers.wandb.WandbLogger)
+            for i in model_trainer.logger
+        ]
+    )
+
+    # disable ckpt, check if ckpt is created
+    files = os.listdir(config.trainer_config.save_ckpt_path)
+    yaml = False
+    for i in files:
+        if i.endswith(".yaml"):
+            yaml = True
+        assert not i.endswith(".ckpt")
+    # check if yaml file is created
+    assert yaml
+
+    # update save_ckpt to True
+    OmegaConf.update(config, "trainer_config.save_ckpt", True)
+    OmegaConf.update(config, "trainer_config.use_wandb", True)
+    model_trainer = ModelTrainer(config)
+    model_trainer._set_wandb()
+    assert wandb.run is not None
+    model_trainer.train()
+    assert any(
+        [
+            isinstance(i, L.pytorch.loggers.wandb.WandbLogger)
+            for i in model_trainer.logger
+        ]
+    )
+
+    files = os.listdir(config.trainer_config.save_ckpt_path)
+    ckpt = False
+    for i in files:
+        if i.endswith("ckpt"):
+            ckpt = True
+            break
+    # check if ckpt is created
+    assert ckpt
+    checkpoint = torch.load(os.path.join(config.trainer_config.save_ckpt_path, i))
+    assert checkpoint["epoch"] == 1
+    labels = sio.load_slp("minimal_instance.pkg.slp")
+    # check if skeleton is saved in ckpt file
+    assert checkpoint["skeleton"] == labels.skeletons
+
+    # check for training metrics csv
+    files = os.listdir(
+        os.path.join(config.trainer_config.save_ckpt_path, "lightning_logs/version_0/")
+    )
+    assert "metrics.csv" in files
+    df = pd.read_csv(
+        os.path.join(
+            config.trainer_config.save_ckpt_path, "lightning_logs/version_0/metrics.csv"
+        )
+    )
+    assert (
+        abs(df.loc[0, "learning_rate"] - config.trainer_config.optimizer.learning_rate)
+        <= 1e-4
+    )
+    assert not df.val_loss.isnull().all()
+    assert not df.train_loss.isnull().all()
+
+
+def test_topdown_centered_instance_model(config):
+
+    model = TopDownCenteredInstanceModel(config)
+    model_trainer = ModelTrainer(config)
+    model_trainer._create_data_loaders()
+    input_ = next(iter(model_trainer.train_data_loader))
+    input_cm = input_["confidence_maps"]
+    preds = model(input_)
+    # check the output shape
+    assert preds.shape == (1, 2, 80, 80)
+
+    # check the loss
+    loss = model.training_step(input_, 0)
+    assert abs(loss - mse_loss(preds, input_cm)) < 1e-3
+
+    # check if cuda is activated
+    OmegaConf.update(config, "trainer_config.device", "cuda")
+    model = TopDownCenteredInstanceModel(config)
+    assert "cuda" in str(model.device)
