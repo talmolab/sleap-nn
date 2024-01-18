67b8027a14028947e93fd855f88e0def6d35b563
alckasoc <tuvincent0106@gmail.com>
2024-01-18 01:49:13 -0800

    added distance_to_edge

--- sleap_nn/data/edge_maps.py
+++ sleap_nn/data/edge_maps.py
@@ -1,3 +1,55 @@
 """Transformers for generating edge confidence maps and part affinity fields."""
 
-"""Transformers for generating edge confidence maps and part affinity fields."""
+import torch
+
+from sleap_nn.data.utils import (
+    expand_to_rank,
+    make_grid_vectors,
+    ensure_list,
+    gaussian_pdf
+)
+
+def distance_to_edge(
+    points: torch.Tensor, edge_source: torch.Tensor, edge_destination: torch.Tensor
+) -> torch.Tensor:
+    # Ensure all points are at least rank 2.
+    points = expand_to_rank(points, 2)
+    edge_source = expand_to_rank(edge_source, 2)
+    edge_destination = expand_to_rank(edge_destination, 2)
+
+    # Compute number of point dimensions.
+    n_pt_dims = points.dim() - 1
+
+    # Direction vector.
+    direction_vector = edge_destination - edge_source  # (n_edges, 2)
+
+    # Edge length.
+    edge_length = torch.maximum(
+        direction_vector.square().sum(dim=1), torch.tensor(1.0)
+    )  # (n_edges,)
+
+    # Adjust query points relative to edge source point.
+    source_relative_points = torch.unsqueeze(points, dim=-2) - expand_to_rank(
+        edge_source, n_pt_dims + 2
+    )  # (..., n_edges, 2)
+
+    # Project points to edge line.
+    line_projections = torch.sum(
+        source_relative_points * expand_to_rank(direction_vector, n_pt_dims + 2), dim=3
+    ) / expand_to_rank(
+        edge_length, n_pt_dims + 1
+    )  # (..., n_edges)
+
+    # Crop to line segment.
+    line_projections = torch.clamp(line_projections, min=0, max=1)
+
+    # Compute distance from each point to the edge.
+    distances = torch.sum(
+        torch.square(
+            (line_projections.unsqueeze(-1) * expand_to_rank(direction_vector, n_pt_dims + 2))
+            - source_relative_points
+        ),
+        dim=-1
+    )  # (..., n_edges)
+
+    return distances
