1b76ef0e0ccecbc6a83079187ddbfc7a71e81fb8
gitttt-1234 <divyasesh11@gmail.com>
2024-09-11 12:33:38 -0700

    Add get chunks functions

--- sleap_nn/data/get_data_chunks.py
+++ sleap_nn/data/get_data_chunks.py
@@ -1,69 +1,160 @@
 """Handles generating data chunks for training."""
 
+from omegaconf import DictConfig
+from typing import Dict, Optional, Tuple, Union
 import numpy as np
 import torch
 
+import sleap_io as sio
 from sleap_nn.data.instance_centroids import generate_centroids
 from sleap_nn.data.instance_cropping import generate_crops
-from sleap_nn.data.normalization import apply_normalization, convert_to_grayscale, convert_to_rgb
+from sleap_nn.data.normalization import (
+    apply_normalization,
+    convert_to_grayscale,
+    convert_to_rgb,
+)
 from sleap_nn.data.providers import process_lf
 from sleap_nn.data.resizing import apply_pad_to_stride, apply_resizer, apply_sizematcher
 
-def bottomup_data_chunks(lf):
 
-    image, instances_n, frame_idx, video_idx = lf
+def bottomup_data_chunks(
+    x: Tuple[Union[sio.LabeledFrame, int]],
+    data_config: DictConfig,
+    max_instances: int,
+    max_stride: int,
+    user_instances_only: bool = True,
+) -> Dict[str, torch.Tensor]:
+    """Generate dict from `sio.LabeledFrame`.
+
+    This function processes the input `sio.LabeledFrame`, applies data pre-processing
+    operations (except augmentation and confmaps generation). This function is passed
+    to `litdata.optimize()` which applies this function on all the `sio.LabeledFrame`s
+    in the training `.slp` file and saves these dictionaries as `.bin` files.
+
+    Args:
+        x: Tuple (lf, video_idx) where lf is a `sio.LabeledFrame` and video_idx is the
+            index of lf.video in the source sio.labels.videos.
+        data_config: Data-related configuration. (`data_config` section in the config file)
+        max_instances: Maximum number of instances that could occur in a single LabeledFrame.
+        max_stride: Scalar integer specifying the maximum stride that the image must be
+            divisible by.
+        user_instances_only: True if filter labels only to user instances else False.
+            Default: True.
+
+    Returns:
+        Dict with image, instances, frame index, video index, original image size and
+        number of instances.
+
+    """
+    lf, video_idx = x
 
     sample = process_lf(lf, video_idx, max_instances, user_instances_only)
 
     # Normalize image
     sample["image"] = apply_normalization(sample["image"])
-    sample["image"] = convert_to_grayscale(sample["image"]) # TODO: how to set config params
-    sample["image"] = convert_to_rgb(sample["image"])
+
+    if data_config.preprocessing.is_rgb:
+        sample["image"] = convert_to_rgb(sample["image"])
+    else:
+        sample["image"] = convert_to_grayscale(sample["image"])
 
     # size matcher
-    sample["image"] = apply_sizematcher(sample["image"], max_height, max_width)
+    sample["image"] = apply_sizematcher(
+        sample["image"],
+        max_height=data_config.preprocessing.max_height,
+        max_width=data_config.preprocessing.max_width,
+    )
 
     # resize the image
-    sample["image"], sample["instances"] = apply_resizer(sample["image"], sample["instances"],
-                                                         scale=scale)
-    
+    sample["image"], sample["instances"] = apply_resizer(
+        sample["image"], sample["instances"], scale=data_config.preprocessing.scale
+    )
+
     # Pad the image (if needed) according max stride
-    sample["image"] = apply_pad_to_stride(sample["image"], max_stride)
+    sample["image"] = apply_pad_to_stride(sample["image"], max_stride=max_stride)
 
     return sample
 
-def centered_data_chunks(lf):
 
-    image, instances_n, frame_idx, video_idx = lf
+def centered_instance_data_chunks(
+    x: Tuple[Union[sio.LabeledFrame, int]],
+    data_config: DictConfig,
+    max_instances: int,
+    crop_size: Tuple[int],
+    anchor_ind: Optional[int],
+    max_stride: int,
+    user_instances_only: bool = True,
+) -> Dict[str, torch.Tensor]:
+    """Generate dict from `sio.LabeledFrame`.
+
+    This function processes the input `sio.LabeledFrame`, applies data pre-processing
+    operations (except augmentation and confmaps generation). This function is passed
+    to `litdata.optimize()` which applies this function on all the `sio.LabeledFrame`s
+    in the training `.slp` file and saves these dictionaries as `.bin` files.
+
+    Args:
+        x: Tuple (lf, video_idx) where lf is a `sio.LabeledFrame` and video_idx is the
+            index of lf.video in the source sio.labels.videos.
+        data_config: Data-related configuration. (`data_config` section in the config file)
+        max_instances: Maximum number of instances that could occur in a single LabeledFrame.
+        crop_size: Height and width of the crop in pixels.
+        anchor_ind: The index of the node to use as the anchor for the centroid. If not
+            provided or if not present in the instance, the midpoint of the bounding box
+            is used instead.
+        max_stride: Scalar integer specifying the maximum stride that the image must be
+            divisible by.
+        user_instances_only: True if filter labels only to user instances else False.
+            Default: True.
+
+    Returns:
+        Dict with image, instances, frame index, video index, original image size and
+        number of instances.
+
+    """
+    lf, video_idx = x
 
     sample = process_lf(lf, video_idx, max_instances, user_instances_only)
 
     # Normalize image
     sample["image"] = apply_normalization(sample["image"])
-    sample["image"] = convert_to_grayscale(sample["image"]) # TODO: how to set config params
-    sample["image"] = convert_to_rgb(sample["image"])
+
+    if data_config.preprocessing.is_rgb:
+        sample["image"] = convert_to_rgb(sample["image"])
+    else:
+        sample["image"] = convert_to_grayscale(sample["image"])
 
     # size matcher
-    sample["image"] = apply_sizematcher(sample["image"], max_height, max_width)
-
-    # resize the image
-    sample["image"], sample["instances"] = apply_resizer(sample["image"], sample["instances"],
-                                                         scale=scale)
-    
-    # Pad the image (if needed) according max stride
-    sample["image"] = apply_pad_to_stride(sample["image"], max_stride)
+    sample["image"] = apply_sizematcher(
+        sample["image"],
+        max_height=data_config.preprocessing.max_height,
+        max_width=data_config.preprocessing.max_width,
+    )
 
     # get the centroids based on the anchor idx
-    centroids = generate_centroids(sample["instances"], anchor_ind = anchor_ind)
+    centroids = generate_centroids(sample["instances"], anchor_ind=anchor_ind)
 
-    crop_size = np.array(crop_size) * np.sqrt(2) # crop extra
+    crop_size = np.array(crop_size) * np.sqrt(2)  # crop extra
     crop_size = crop_size.astype(np.int32).tolist()
 
-    for cnt, (instance, centroid) in enumerate(zip(sample["instances"], sample["centroids"])):
-        if cnt==sample["num_instances"]:
+    sample["instances"], centroids = sample["instances"][0], centroids[0]  # n_samples=1
+
+    for cnt, (instance, centroid) in enumerate(zip(sample["instances"], centroids)):
+        if cnt == sample["num_instances"]:
             break
 
         res = generate_crops(sample["image"], instance, centroid, crop_size)
+
+        # resize the image
+        res["instance_image"], res["instance"] = apply_resizer(
+            res["instance_image"],
+            res["instance"],
+            scale=data_config.preprocessing.scale,
+        )
+
+        # Pad the image (if needed) according max stride
+        res["instance_image"] = apply_pad_to_stride(
+            res["instance_image"], max_stride=max_stride
+        )
 
         res["frame_idx"] = sample["frame_idx"]
         res["video_idx"] = sample["video_idx"]
@@ -72,53 +163,129 @@ def centered_data_chunks(lf):
 
         yield res
 
-def centroid_data_chunks(lf):
 
-    image, instances_n, frame_idx, video_idx = lf
+def centroid_data_chunks(
+    x: Tuple[Union[sio.LabeledFrame, int]],
+    data_config: DictConfig,
+    max_instances: int,
+    anchor_ind: Optional[int],
+    max_stride: int,
+    user_instances_only: bool = True,
+) -> Dict[str, torch.Tensor]:
+    """Generate dict from `sio.LabeledFrame`.
+
+    This function processes the input `sio.LabeledFrame`, applies data pre-processing
+    operations (except augmentation and confmaps generation). This function is passed
+    to `litdata.optimize()` which applies this function on all the `sio.LabeledFrame`s
+    in the training `.slp` file and saves these dictionaries as `.bin` files.
+
+    Args:
+        x: Tuple (lf, video_idx) where lf is a `sio.LabeledFrame` and video_idx is the
+            index of lf.video in the source sio.labels.videos.
+        data_config: Data-related configuration. (`data_config` section in the config file)
+        max_instances: Maximum number of instances that could occur in a single LabeledFrame.
+        anchor_ind: The index of the node to use as the anchor for the centroid. If not
+            provided or if not present in the instance, the midpoint of the bounding box
+            is used instead.
+        max_stride: Scalar integer specifying the maximum stride that the image must be
+            divisible by.
+        user_instances_only: True if filter labels only to user instances else False.
+            Default: True.
+
+    Returns:
+        Dict with image, instances, frame index, video index, original image size and
+        number of instances.
+
+    """
+    lf, video_idx = x
 
     sample = process_lf(lf, video_idx, max_instances, user_instances_only)
 
     # Normalize image
     sample["image"] = apply_normalization(sample["image"])
-    sample["image"] = convert_to_grayscale(sample["image"]) # TODO: how to set config params
-    sample["image"] = convert_to_rgb(sample["image"])
+
+    if data_config.preprocessing.is_rgb:
+        sample["image"] = convert_to_rgb(sample["image"])
+    else:
+        sample["image"] = convert_to_grayscale(sample["image"])
 
     # size matcher
-    sample["image"] = apply_sizematcher(sample["image"], max_height, max_width)
+    sample["image"] = apply_sizematcher(
+        sample["image"],
+        max_height=data_config.preprocessing.max_height,
+        max_width=data_config.preprocessing.max_width,
+    )
 
     # resize the image
-    sample["image"], sample["instances"] = apply_resizer(sample["image"], sample["instances"],
-                                                         scale=scale)
-    
+    sample["image"], sample["instances"] = apply_resizer(
+        sample["image"], sample["instances"], scale=data_config.preprocessing.scale
+    )
+
     # Pad the image (if needed) according max stride
-    sample["image"] = apply_pad_to_stride(sample["image"], max_stride)
+    sample["image"] = apply_pad_to_stride(sample["image"], max_stride=max_stride)
 
     # get the centroids based on the anchor idx
-    centroids = generate_centroids(sample["instances"], anchor_ind = anchor_ind)
+    centroids = generate_centroids(sample["instances"], anchor_ind=anchor_ind)
 
     sample["centroids"] = centroids
 
     return sample
 
-def single_instance_data_chunks(lf):
 
-    image, instances_n, frame_idx, video_idx = lf
+def single_instance_data_chunks(
+    x: Tuple[Union[sio.LabeledFrame, int]],
+    data_config: DictConfig,
+    max_stride: int,
+    user_instances_only: bool = True,
+) -> Dict[str, torch.Tensor]:
+    """Generate dict from `sio.LabeledFrame`.
 
-    sample = process_lf(lf, video_idx, max_instances, user_instances_only)
+    This function processes the input `sio.LabeledFrame`, applies data pre-processing
+    operations (except augmentation and confmaps generation). This function is passed
+    to `litdata.optimize()` which applies this function on all the `sio.LabeledFrame`s
+    in the training `.slp` file and saves these dictionaries as `.bin` files.
+
+    Args:
+        x: Tuple (lf, video_idx) where lf is a `sio.LabeledFrame` and video_idx is the
+            index of lf.video in the source sio.labels.videos.
+        data_config: Data-related configuration. (`data_config` section in the config file)
+        max_stride: Scalar integer specifying the maximum stride that the image must be
+            divisible by.
+        user_instances_only: True if filter labels only to user instances else False.
+            Default: True.
+
+    Returns:
+        Dict with image, instances, frame index, video index, original image size and
+        number of instances.
+
+    """
+    lf, video_idx = x
+
+    sample = process_lf(
+        lf, video_idx, user_instances_only=user_instances_only, max_instances=1
+    )
 
     # Normalize image
     sample["image"] = apply_normalization(sample["image"])
-    sample["image"] = convert_to_grayscale(sample["image"]) # TODO: how to set config params
-    sample["image"] = convert_to_rgb(sample["image"])
+
+    if data_config.preprocessing.is_rgb:
+        sample["image"] = convert_to_rgb(sample["image"])
+    else:
+        sample["image"] = convert_to_grayscale(sample["image"])
 
     # size matcher
-    sample["image"] = apply_sizematcher(sample["image"], max_height, max_width)
+    sample["image"] = apply_sizematcher(
+        sample["image"],
+        max_height=data_config.preprocessing.max_height,
+        max_width=data_config.preprocessing.max_width,
+    )
 
     # resize the image
-    sample["image"], sample["instances"] = apply_resizer(sample["image"], sample["instances"],
-                                                         scale=scale)
-    
+    sample["image"], sample["instances"] = apply_resizer(
+        sample["image"], sample["instances"], scale=data_config.preprocessing.scale
+    )
+
     # Pad the image (if needed) according max stride
-    sample["image"] = apply_pad_to_stride(sample["image"], max_stride)
+    sample["image"] = apply_pad_to_stride(sample["image"], max_stride=max_stride)
 
     return sample
---
+++ tests/data/test_get_data_chunks.py
@@ -0,0 +1,247 @@
+import sleap_io as sio
+
+from sleap_nn.data.get_data_chunks import (
+    bottomup_data_chunks,
+    centered_instance_data_chunks,
+    centroid_data_chunks,
+    single_instance_data_chunks,
+)
+
+
+def test_bottomup_data_chunks(minimal_instance, config):
+    """Test `bottomup_data_chunks` function."""
+    labels = sio.load_slp(minimal_instance)
+    samples = []
+    for idx, lf in enumerate(labels):
+        samples.append(
+            bottomup_data_chunks(
+                (lf, idx), data_config=config.data_config, max_instances=4, max_stride=2
+            )
+        )
+
+    assert len(samples) == 1
+
+    gt_keys = [
+        "image",
+        "instances",
+        "frame_idx",
+        "video_idx",
+        "num_instances",
+        "orig_size",
+    ]
+    for k in gt_keys:
+        assert k in samples[0]
+
+    assert samples[0]["image"].shape == (1, 1, 384, 384)
+    assert samples[0]["instances"].shape == (1, 4, 2, 2)
+
+    # test pad_to_stride
+    samples = []
+    for idx, lf in enumerate(labels):
+        samples.append(
+            bottomup_data_chunks(
+                (lf, idx),
+                data_config=config.data_config,
+                max_instances=2,
+                max_stride=100,
+            )
+        )
+
+    gt_keys = [
+        "image",
+        "instances",
+        "frame_idx",
+        "video_idx",
+        "num_instances",
+        "orig_size",
+    ]
+    for k in gt_keys:
+        assert k in samples[0]
+
+    assert samples[0]["image"].shape == (1, 1, 400, 400)
+    assert samples[0]["instances"].shape == (1, 2, 2, 2)
+
+
+def test_centered_instance_data_chunks(minimal_instance, config):
+    """Test `centered_instance_data_chunks` function."""
+    labels = sio.load_slp(minimal_instance)
+    samples = []
+    config.data_config.preprocessing.scale = 2
+    for idx, lf in enumerate(labels):
+        res = centered_instance_data_chunks(
+            (lf, idx),
+            data_config=config.data_config,
+            anchor_ind=0,
+            crop_size=(160, 160),
+            max_instances=2,
+            max_stride=2,
+        )
+        samples.extend(res)
+
+    assert len(samples) == 2
+
+    gt_keys = [
+        "instance_image",
+        "instance",
+        "frame_idx",
+        "video_idx",
+        "num_instances",
+        "orig_size",
+        "instance_bbox",
+        "centroid",
+    ]
+    for k in gt_keys:
+        assert k in samples[0]
+
+    assert samples[0]["instance_image"].shape == (1, 1, 452, 452)
+    assert samples[0]["instance"].shape == (1, 2, 2)
+
+    # test pad_to_stride
+    config.data_config.preprocessing.scale = 1.0
+    samples = []
+    for idx, lf in enumerate(labels):
+        res = centered_instance_data_chunks(
+            (lf, idx),
+            data_config=config.data_config,
+            anchor_ind=0,
+            crop_size=(160, 160),
+            max_instances=2,
+            max_stride=100,
+        )
+        samples.extend(res)
+
+    gt_keys = [
+        "instance_image",
+        "instance",
+        "frame_idx",
+        "video_idx",
+        "num_instances",
+        "orig_size",
+        "instance_bbox",
+        "centroid",
+    ]
+    for k in gt_keys:
+        assert k in samples[0]
+
+    assert samples[0]["instance_image"].shape == (1, 1, 300, 300)
+    assert samples[0]["instance"].shape == (1, 2, 2)
+
+
+def test_centroid_data_chunks(minimal_instance, config):
+    """Test `centroid_data_chunks` function."""
+    labels = sio.load_slp(minimal_instance)
+    samples = []
+    config.data_config.preprocessing.scale = 0.5
+    for idx, lf in enumerate(labels):
+        samples.append(
+            centroid_data_chunks(
+                (lf, idx),
+                data_config=config.data_config,
+                max_instances=4,
+                max_stride=2,
+                anchor_ind=0,
+            )
+        )
+
+    assert len(samples) == 1
+
+    gt_keys = [
+        "image",
+        "instances",
+        "frame_idx",
+        "video_idx",
+        "num_instances",
+        "orig_size",
+        "centroids",
+    ]
+    for k in gt_keys:
+        assert k in samples[0]
+
+    assert samples[0]["image"].shape == (1, 1, 192, 192)
+    assert samples[0]["instances"].shape == (1, 4, 2, 2)
+    assert samples[0]["centroids"].shape == (1, 4, 2)
+
+    # test pad_to_stride
+    samples = []
+    config.data_config.preprocessing.scale = 1.0
+    for idx, lf in enumerate(labels):
+        samples.append(
+            centroid_data_chunks(
+                (lf, idx),
+                data_config=config.data_config,
+                max_instances=2,
+                anchor_ind=0,
+                max_stride=100,
+            )
+        )
+
+    gt_keys = [
+        "image",
+        "instances",
+        "frame_idx",
+        "video_idx",
+        "num_instances",
+        "orig_size",
+        "centroids",
+    ]
+    for k in gt_keys:
+        assert k in samples[0]
+
+    assert samples[0]["image"].shape == (1, 1, 400, 400)
+    assert samples[0]["instances"].shape == (1, 2, 2, 2)
+    assert samples[0]["centroids"].shape == (1, 2, 2)
+
+
+def test_single_instance_data_chunks(minimal_instance, config):
+    """Test `single_instance_data_chunks` function."""
+    labels = sio.load_slp(minimal_instance)
+    # Making our minimal 2-instance example into a single instance example.
+    for lf in labels:
+        lf.instances = lf.instances[:1]
+
+    samples = []
+    for idx, lf in enumerate(labels):
+        samples.append(
+            single_instance_data_chunks(
+                (lf, idx), data_config=config.data_config, max_stride=2
+            )
+        )
+
+    assert len(samples) == 1
+
+    gt_keys = [
+        "image",
+        "instances",
+        "frame_idx",
+        "video_idx",
+        "num_instances",
+        "orig_size",
+    ]
+    for k in gt_keys:
+        assert k in samples[0]
+
+    assert samples[0]["image"].shape == (1, 1, 384, 384)
+    assert samples[0]["instances"].shape == (1, 1, 2, 2)
+
+    # test pad_to_stride
+    samples = []
+    for idx, lf in enumerate(labels):
+        samples.append(
+            single_instance_data_chunks(
+                (lf, idx), data_config=config.data_config, max_stride=100
+            )
+        )
+
+    gt_keys = [
+        "image",
+        "instances",
+        "frame_idx",
+        "video_idx",
+        "num_instances",
+        "orig_size",
+    ]
+    for k in gt_keys:
+        assert k in samples[0]
+
+    assert samples[0]["image"].shape == (1, 1, 400, 400)
+    assert samples[0]["instances"].shape == (1, 1, 2, 2)
