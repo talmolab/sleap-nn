5e77de92dc41eafaa9273a6491f44810b2742fef
DivyaSesh <64513125+gitttt-1234@users.noreply.github.com>
2024-09-11 11:53:11 -0700

    Resume training and automatically compute crop size for TopDownConfmaps pipeline (#79)
    * Add option to automatically compute crop size
    
    * Move find_crop_size to Trainer
    
    * Fix skeleton name
    
    * Add crop size to config
    
    * Add resumable training option
    
    * Add tests fore resuming training
    
    * Fix tests
    
    * Fix test for wandb folder
    
    * LitData Refactor PR1: Get individual functions for data pipelines (#81)
    
    * Add functions for data pipelines
    
    * Add test cases
    
    * Format file
    
    * Add more test cases
    
    * Fix augmentation test

--- sleap_nn/data/instance_cropping.py
+++ sleap_nn/data/instance_cropping.py
@@ -1,11 +1,64 @@
 """Handle cropping of instances."""
 
-from typing import Iterator, Tuple, Dict
-
+from typing import Iterator, Tuple, Dict, Optional
+import math
+import numpy as np
+import sleap_io as sio
 import torch
 from kornia.geometry.transform import crop_and_resize
 from torch.utils.data.datapipes.datapipe import IterDataPipe
-from sleap_nn.data.resizing import find_padding_for_stride
+
+
+def find_instance_crop_size(
+    labels: sio.Labels,
+    padding: int = 0,
+    maximum_stride: int = 2,
+    input_scaling: float = 1.0,
+    min_crop_size: Optional[int] = None,
+) -> int:
+    """Compute the size of the largest instance bounding box from labels.
+
+    Args:
+        labels: A `sio.Labels` containing user-labeled instances.
+        padding: Integer number of pixels to add to the bounds as margin padding.
+        maximum_stride: Ensure that the returned crop size is divisible by this value.
+            Useful for ensuring that the crop size will not be truncated in a given
+            architecture.
+        input_scaling: Float factor indicating the scale of the input images if any
+            scaling will be done before cropping.
+        min_crop_size: The crop size set by the user.
+
+    Returns:
+        An integer crop size denoting the length of the side of the bounding boxes that
+        will contain the instances when cropped. The returned crop size will be larger
+        or equal to the input `min_crop_size`.
+
+        This accounts for stride, padding and scaling when ensuring divisibility.
+    """
+    # Check if user-specified crop size is divisible by max stride
+    min_crop_size = 0 if min_crop_size is None else min_crop_size
+    if (min_crop_size > 0) and (min_crop_size % maximum_stride == 0):
+        return min_crop_size
+
+    # Calculate crop size
+    min_crop_size_no_pad = min_crop_size - padding
+    max_length = 0.0
+    for lf in labels:
+        for inst in lf.instances:
+            pts = inst.numpy()
+            pts *= input_scaling
+            max_length = np.maximum(
+                max_length, np.nanmax(pts[:, 0]) - np.nanmin(pts[:, 0])
+            )
+            max_length = np.maximum(
+                max_length, np.nanmax(pts[:, 1]) - np.nanmin(pts[:, 1])
+            )
+            max_length = np.maximum(max_length, min_crop_size_no_pad)
+
+    max_length += float(padding)
+    crop_size = math.ceil(max_length / float(maximum_stride)) * maximum_stride
+
+    return int(crop_size)
 
 
 def make_centered_bboxes(
@@ -53,6 +106,54 @@ def make_centered_bboxes(
     return corners + offset
 
 
+def generate_crops(
+    image: torch.Tensor,
+    instance: torch.Tensor,
+    centroid: torch.Tensor,
+    crop_size: Tuple[int],
+) -> Dict[str, torch.Tensor]:
+    """Generate cropped image for the given centroid.
+
+    Args:
+        image: Input source image. (n_samples, C, H, W)
+        instance: Keypoints for the instance to be cropped. (n_nodes, 2)
+        centroid: Centroid of the instance to be cropped. (2)
+        crop_size: (height, width) of the crop to be generated.
+
+    Returns:
+        A dictionary with cropped images, bounding box for the cropped instance, keypoints and
+        centroids adjusted to the crop.
+    """
+    box_size = crop_size
+
+    # Generate bounding boxes from centroid.
+    instance_bbox = torch.unsqueeze(
+        make_centered_bboxes(centroid, box_size[0], box_size[1]), 0
+    )  # (n_samples=1, 4, 2)
+
+    # Generate cropped image of shape (n_samples, C, crop_H, crop_W)
+    instance_image = crop_and_resize(
+        image,
+        boxes=instance_bbox,
+        size=box_size,
+    )
+
+    # Access top left point (x,y) of bounding box and subtract this offset from
+    # position of nodes.
+    point = instance_bbox[0][0]
+    center_instance = (instance - point).unsqueeze(0)  # (n_samples=1, n_nodes, 2)
+    centered_centroid = (centroid - point).unsqueeze(0)  # (n_samples=1, 2)
+
+    cropped_sample = {
+        "instance_image": instance_image,
+        "instance_bbox": instance_bbox,
+        "instance": center_instance,
+        "centroid": centered_centroid,
+    }
+
+    return cropped_sample
+
+
 class InstanceCropper(IterDataPipe):
     """IterDataPipe for cropping instances.
 
@@ -60,7 +161,7 @@ class InstanceCropper(IterDataPipe):
 
     Attributes:
         source_dp: The previous `IterDataPipe` with samples that contain an `instances` key.
-        crop_hw: Minimum height and width of the crop in pixels.
+        crop_hw: Height and width of the crop in pixels.
 
     """
 
