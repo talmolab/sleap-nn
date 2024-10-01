80e5915ddef2ef660e75a8f7b6883496ca8664c2
gqcpm <63070177+gqcpm@users.noreply.github.com>
2024-10-01 16:53:17 -0700

    created all classes, still need to edit last two

--- sleap_nn/config/data_config.py
+++ sleap_nn/config/data_config.py
@@ -1,3 +1,7 @@
+import attr
+from omegaconf import MISSING
+from typing import Optional, Tuple, List, Dict
+
 @attr.s(auto_attribs=True)
 class DataConfig:
     """Data configuration.
@@ -8,6 +12,56 @@ class DataConfig:
         and topdown models.
     """
 
-    labels: LabelsConfig = attr.ib(factory=LabelsConfig)
+    provider: str="LabelsReader"
+    train_labels_path: str=MISSING
+    val_labels_path: str=MISSING
     preprocessing: PreprocessingConfig = attr.ib(factory=PreprocessingConfig)
-    instance_cropping: InstanceCroppingConfig = attr.ib(factory=InstanceCroppingConfig)
+    use_augmentations_train: bool=False
+    augmentation_config: Optional[AugmentationConfig] = None
+
+
+@attr.s(auto_attribs=True)
+class PreprocessingConfig:
+    is_rgb: bool = True
+    max_height: Optional[int] = None
+    max_width: Optional[int] = None
+    scale: Union[float, Tuple[float, float]] = 1.0
+    crop_hw: Optional[Tuple[int, int]] = None
+    min_crop_size: int = 32                  #to help app work incase of error
+
+@attr.s(auto_attribs=True)
+class AugmentationConfig:
+    random_crop: Optional[Dict[str, Optional[float]]] = None
+    intensity: Optional[IntensityConfig] = attr.ib(default=None)
+    geometric: Optional[GeometricConfig] = attr.ib(default=None)
+
+# still looking over this part and below (function to check if is greater than 0)
+@attr.s(auto_attribs=True)
+class IntensityConfig:
+    uniform_noise_min: float = 0.0
+    uniform_noise_max: float = 1.0
+    uniform_noise_p: float = 0.0
+    gaussian_noise_mean: float = 0.0
+    gaussian_noise_std: float = 1.0
+    gaussian_noise_p: float = 0.0
+    contrast_min: float = 0.5
+    contrast_max: float = 2.0
+    contrast_p: float = 0.0
+    brightness: Tuple[float, float] = (1.0, 1.0)
+    brightness_p: float = 0.0
+
+@attr.s(auto_attribs=True)
+class GeometricConfig:
+    rotation: float = 0.0
+    scale: Optional[Tuple[float, float, float, float]] = None
+    translate_width: float = 0.0
+    translate_height: float = 0.0
+    affine_p: float = 0.0
+    erase_scale_min: float = 0.0001
+    erase_scale_max: float = 0.01
+    erase_ratio_min: float = 1.0
+    erase_ratio_max: float = 1.0
+    erase_p: float = 0.0
+    mixup_lambda: Optional[float] = None
+    mixup_p: float = 0.0
+    input_key: str = "image"
