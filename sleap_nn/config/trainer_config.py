a074821fbc2810b84f2ea593538abdc810ee8944
gqcpm <63070177+gqcpm@users.noreply.github.com>
2024-10-21 08:09:53 -0700

    rename and setup for trainerconfig

---
+++ sleap_nn/config/trainer_config.py
@@ -0,0 +1,49 @@
+from omegaconf import OmegaConf
+
+"""Serializable configuration classes for specifying all training job parameters.
+
+These configuration classes are intended to specify all the parameters required to run
+a training job or perform inference from a serialized one.
+
+They are explicitly not intended to implement any of the underlying functionality that
+they parametrize. This serves two purposes:
+
+    1. Parameter specification through simple attributes. These can be read/edited by a
+        human, as well as easily be serialized/deserialized to/from simple dictionaries
+        and JSON.
+
+    2. Decoupling from the implementation. This makes it easier to design functional
+        modules with attributes/parameters that contain objects that may not be easily
+        serializable or may implement additional logic that relies on runtime
+        information or other parameters.
+
+In general, classes that implement the actual functionality related to these
+configuration classes should provide a classmethod for instantiation from the
+configuration class instances. This makes it easier to implement other logic not related
+to the high level parameters at creation time.
+
+Conveniently, this format also provides a single location where all user-facing
+parameters are aggregated and documented for end users (as opposed to developers).
+"""
+
+
+@attrs.define
+class TrainerConfig:
+    """Configuration of Trainer.
+
+    Attributes:
+        train_data_loader: (Note: Any parameters from Torch's DataLoader could be used.) 
+        val_data_loader: (Similar to train_data_loader)
+        model_ckpt: (Note: Any parameters from Lightning's ModelCheckpoint could be used.) 
+        trainer_devices: (int) Number of devices to train on (int), which devices to train on (list or str), or "auto" to select automatically.
+        trainer_accelerator: (str) One of the ("cpu", "gpu", "tpu", "ipu", "auto"). "auto" recognises the machine the model is running on and chooses the appropriate accelerator for the Trainer to be connected to.
+        enable_progress_bar: (bool) When True, enables printing the logs during training.
+        steps_per_epoch: (int) Minimum number of iterations in a single epoch. (Useful if model is trained with very few data points). Refer limit_train_batches parameter of Torch Trainer. If None, the number of iterations depends on the number of samples in the train dataset.
+        max_epochs: (int) Maxinum number of epochs to run.
+        seed: (int) Seed value for the current experiment.
+        use_wandb: (bool) True to enable wandb logging.
+        save_ckpt: (bool) True to enable checkpointing.
+        save_ckpt_path: (str) Directory path to save the training config and checkpoint files. Default: "./"
+        resume_ckpt_path: (str) Path to .ckpt file from which training is resumed. Default: None.
+        wandb: (Only if use_wandb is True, else skip this)
+    """
