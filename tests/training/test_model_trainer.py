a88286102c25547b09b4aaaf12736189b959ed65
gitttt-1234 <divyasesh11@gmail.com>
2024-10-29 10:01:10 -0700

    Test 1: first test with shutil

--- tests/training/test_model_trainer.py
+++ tests/training/test_model_trainer.py
@@ -70,67 +70,67 @@ def test_trainer(config, tmp_path: str, minimal_instance_bottomup_ckpt: str):
     model_trainer = ModelTrainer(config)
     assert model_trainer.dir_path == "."
 
-    #####
-
-    # # for topdown centered instance model
     OmegaConf.update(
         config, "trainer_config.save_ckpt_path", f"{tmp_path}/test_model_trainer/"
     )
-    # model_trainer = ModelTrainer(config)
-    # model_trainer.train()
 
-    # # disable ckpt, check if ckpt is created
-    # folder_created = Path(config.trainer_config.save_ckpt_path).exists()
-    # assert (
-    #     Path(config.trainer_config.save_ckpt_path)
-    #     .joinpath("training_config.yaml")
-    #     .exists()
-    # )
-    # assert not (
-    #     Path(config.trainer_config.save_ckpt_path).joinpath("best.ckpt").exists()
-    # )
+    #####
 
-    #######
-
-    # update save_ckpt to True
-    OmegaConf.update(config, "trainer_config.save_ckpt", False)
-    OmegaConf.update(config, "trainer_config.use_wandb", False)
-    OmegaConf.update(config, "data_config.preprocessing.crop_hw", None)
-    OmegaConf.update(config, "data_config.preprocessing.min_crop_size", 100)
-
+    # # for topdown centered instance model
     model_trainer = ModelTrainer(config)
     model_trainer.train()
 
-    # check if wandb folder is created
-    # assert Path(config.trainer_config.save_ckpt_path).joinpath("wandb").exists()
-
-    folder_created = Path(config.trainer_config.save_ckpt_path).exists()
-    assert folder_created
-    # files = [
-    #     str(x)
-    #     for x in Path(config.trainer_config.save_ckpt_path).iterdir()
-    #     if x.is_file()
-    # ]
-    assert (
-        Path(config.trainer_config.save_ckpt_path)
-        .joinpath("initial_config.yaml")
-        .exists()
-    )
+    # disable ckpt, check if ckpt is created
     assert (
         Path(config.trainer_config.save_ckpt_path)
         .joinpath("training_config.yaml")
         .exists()
     )
-    training_config = OmegaConf.load(
-        f"{config.trainer_config.save_ckpt_path}/training_config.yaml"
+    assert not (
+        Path(config.trainer_config.save_ckpt_path).joinpath("best.ckpt").exists()
     )
-    assert training_config.trainer_config.wandb.run_id is not None
-    assert training_config.model_config.total_params is not None
-    # assert training_config.trainer_config.wandb.api_key == ""
-    assert training_config.data_config.skeletons
-    assert training_config.data_config.preprocessing.crop_hw == (104, 104)
 
-    # check if ckpt is created
+    #######
+
+    # update save_ckpt to True
+    # OmegaConf.update(config, "trainer_config.save_ckpt", True)
+    # OmegaConf.update(config, "trainer_config.use_wandb", True)
+    # OmegaConf.update(config, "data_config.preprocessing.crop_hw", None)
+    # OmegaConf.update(config, "data_config.preprocessing.min_crop_size", 100)
+
+    # model_trainer = ModelTrainer(config)
+    # model_trainer.train()
+
+    # # check if wandb folder is created
+    # assert Path(config.trainer_config.save_ckpt_path).joinpath("wandb").exists()
+
+    # folder_created = Path(config.trainer_config.save_ckpt_path).exists()
+    # assert folder_created
+    # files = [
+    #     str(x)
+    #     for x in Path(config.trainer_config.save_ckpt_path).iterdir()
+    #     if x.is_file()
+    # ]
+    # assert (
+    #     Path(config.trainer_config.save_ckpt_path)
+    #     .joinpath("initial_config.yaml")
+    #     .exists()
+    # )
+    # assert (
+    #     Path(config.trainer_config.save_ckpt_path)
+    #     .joinpath("training_config.yaml")
+    #     .exists()
+    # )
+    # training_config = OmegaConf.load(
+    #     f"{config.trainer_config.save_ckpt_path}/training_config.yaml"
+    # )
+    # assert training_config.trainer_config.wandb.run_id is not None
+    # assert training_config.model_config.total_params is not None
+    # assert training_config.trainer_config.wandb.api_key == ""
+    # assert training_config.data_config.skeletons
+    # assert training_config.data_config.preprocessing.crop_hw == (104, 104)
+
+    # # check if ckpt is created
     # assert Path(config.trainer_config.save_ckpt_path).joinpath("last.ckpt").exists()
     # assert Path(config.trainer_config.save_ckpt_path).joinpath("best.ckpt").exists()
 
@@ -139,12 +139,12 @@ def test_trainer(config, tmp_path: str, minimal_instance_bottomup_ckpt: str):
     # )
     # assert checkpoint["epoch"] == 1
 
-    # check if skeleton is saved in ckpt file
+    # # check if skeleton is saved in ckpt file
     # assert checkpoint["config"]
     # assert checkpoint["config"]["trainer_config"]["wandb"]["api_key"] == ""
     # assert len(checkpoint["config"]["data_config"]["skeletons"].keys()) == 1
 
-    # check for training metrics csv
+    # # check for training metrics csv
     # path = Path(config.trainer_config.save_ckpt_path).joinpath(
     #     "lightning_logs/version_0/"
     # )
