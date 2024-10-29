19f52568ecb0bcf03aad34a8db608e13d2a2c9e3
gqcpm <63070177+gqcpm@users.noreply.github.com>
2024-10-28 20:40:58 -0700

    instantiate backboneconfig with oneof decorator

--- sleap_nn/config/model_config.py
+++ sleap_nn/config/model_config.py
@@ -9,34 +9,18 @@ class ModelConfig:
     Attributes:
         init_weight: (str) model weights initialization method. "default" uses kaiming uniform initialization and "xavier" uses Xavier initialization method.
         pre_trained_weights: (str) Pretrained weights file name supported only for ConvNext and SwinT backbones. For ConvNext, one of ["ConvNeXt_Base_Weights","ConvNeXt_Tiny_Weights", "ConvNeXt_Small_Weights", "ConvNeXt_Large_Weights"]. For SwinT, one of ["Swin_T_Weights", "Swin_S_Weights", "Swin_B_Weights"].
-        backbone_type: (str) Backbone architecture for the model to be trained. One of "unet", "convnext" or "swint".
         backbone_config: initialize either UNetConfig, ConvNextConfig, or SwinTConfig based on input from backbone_type
         head_config: head_configs: (Dict) Dictionary with the following keys having head configs for the model to be trained. Note: Configs should be provided only for the model to train and others should be None
     """
 
     init_weight: str = "default"
     pre_trained_weights: str = None
-    backbone_type: BackboneType = BackboneType.UNET
-    backbone_config: Union[UNetConfig, ConvNextConfig, SwinTConfig] = attrs.field(
-        init=False
-    )  # backbone_config can be any of these 3 configurations. init=False lets you set the parameters later (not in initialization)
+    backbone_config: BackboneConfig = attrs.field(factory=BackboneConfig)
     head_configs: HeadConfig = attrs.field(factory=HeadConfig)
 
     # post-initialization
     def __attrs_post_init__(self):
-        self.backbone_config = self.set_backbone_config()
         self.validate_pre_trained_weights()
-
-    # configures back_bone config to one of these types
-    def set_backbone_config(self):
-        if self.backbone_type == BackboneType.UNET:
-            return UNetConfig()
-        elif self.backbone_type == BackboneType.CONVNEXT:
-            return ConvNextConfig()
-        elif self.backbone_type == BackboneType.SWINT:
-            return SwinTConfig()
-        else:
-            raise ValueError(f"Invalid backbone_type: {self.backbone_type}")
 
     # validate the pre-trained weights
     def validate_pre_trained_weights(self):
@@ -64,107 +48,121 @@ class ModelConfig:
         ):
             raise ValueError("UNet does not support pre-trained weights.")
 
-    class BackboneType(Enum):
-        UNET = "unet"
-        CONVNEXT = "convnext"
-        SWINT = "swint"
 
-    # Define configuration for each backbone type
-    @attrs.define
-    class UNetConfig:
-        """unet config for backbone
+@oneof
+@attrs.define
+class BackboneConfig:
+    """Configurations related to model backbone configuration.
 
-        Attributes:
-            in_channels: (int) Number of input channels. Default is 1.
-            kernel_size: (int) Size of the convolutional kernels. Default is 3.
-            filters: (int) Base number of filters in the network. Default is 32
-            filters_rate: (float) Factor to adjust the number of filters per block. Default is 1.5.
-            max_stride: (int) Scalar integer specifying the maximum stride that the image must be divisible by.
-            stem_stride: (int) If not None, will create additional "down" blocks for initial downsampling based on the stride. These will be configured identically to the down blocks below.
-            middle_block: (bool) If True, add an additional block at the end of the encoder. default: True
-            up_interpolate: (bool) If True, use bilinear interpolation instead of transposed convolutions for upsampling. Interpolation is faster but transposed convolutions may be able to learn richer or more complex upsampling to recover details from higher scales. Default: True.
-            stacks: (int) Number of upsampling blocks in the decoder. Default is 3.
-            convs_per_block: (int) Number of convolutional layers per block. Default is 2.
-        """
+    Attributes:
+        unet: An instance of `UNetConfig`.
+        convnext: An instance of `ConvNextConfig`.
+        swint: An instance of `SwinTConfig`.
+    """
 
-        in_channels: int = 1
-        kernel_size: int = 3
-        filters: int = 32
-        filters_rate: float = 1.5
-        max_stride: int = None
-        stem_stride: int = None
-        middle_block: bool = True
-        up_interpolate: bool = True
-        stacks: int = 3
-        convs_per_block: int = 2
+    unet: Optional[UNetConfig] = None
+    convnext: Optional[ConvNextConfig] = None
+    swint: Optional[SwinTConfig] = None
 
-    @attrs.define
-    class ConvNextConfig:
-        """convnext configuration for backbone
 
-        Attributes:
-            arch: (Default is Tiny architecture config. No need to provide if model_type is provided)
-                depths: (List(int)) Number of layers in each block. Default: [3, 3, 9, 3].
-                channels: (List(int)) Number of channels in each block. Default: [96, 192, 384, 768].
-            model_type: (str) One of the ConvNext architecture types: ["tiny", "small", "base", "large"]. Default: "tiny".
-            stem_patch_kernel: (int) Size of the convolutional kernels in the stem layer. Default is 4.
-            stem_patch_stride: (int) Convolutional stride in the stem layer. Default is 2.
-            in_channels: (int) Number of input channels. Default is 1.
-            kernel_size: (int) Size of the convolutional kernels. Default is 3.
-            filters_rate: (float) Factor to adjust the number of filters per block. Default is 1.5.
-            convs_per_block: (int) Number of convolutional layers per block. Default is 2.
-            up_interpolate: (bool) If True, use bilinear interpolation instead of transposed convolutions for upsampling. Interpolation is faster but transposed convolutions may be able to learn richer or more complex upsampling to recover details from higher scales. Default: True.
-        """
+# Define configuration for each backbone type (unet, convnext, swint) configurations
+@attrs.define
+class UNetConfig:
+    """unet config for backbone
 
-        model_type: str = "tiny"  # Options: tiny, small, base, large
-        arch: dict = attrs.field(
-            factory=lambda: {"depths": [3, 3, 9, 3], "channels": [96, 192, 384, 768]}
-        )
-        stem_patch_kernel: int = 4
-        stem_patch_stride: int = 2
-        in_channels: int = 1
-        kernel_size: int = 3
-        filters_rate: float = 1.5
-        convs_per_block: int = 2
-        up_interpolate: bool = True
+    Attributes:
+        in_channels: (int) Number of input channels. Default is 1.
+        kernel_size: (int) Size of the convolutional kernels. Default is 3.
+        filters: (int) Base number of filters in the network. Default is 32
+        filters_rate: (float) Factor to adjust the number of filters per block. Default is 1.5.
+        max_stride: (int) Scalar integer specifying the maximum stride that the image must be divisible by.
+        stem_stride: (int) If not None, will create additional "down" blocks for initial downsampling based on the stride. These will be configured identically to the down blocks below.
+        middle_block: (bool) If True, add an additional block at the end of the encoder. default: True
+        up_interpolate: (bool) If True, use bilinear interpolation instead of transposed convolutions for upsampling. Interpolation is faster but transposed convolutions may be able to learn richer or more complex upsampling to recover details from higher scales. Default: True.
+        stacks: (int) Number of upsampling blocks in the decoder. Default is 3.
+        convs_per_block: (int) Number of convolutional layers per block. Default is 2.
+    """
 
-    @attrs.define
-    class SwinTConfig:
-        """swinT configuration for backbone
+    in_channels: int = 1
+    kernel_size: int = 3
+    filters: int = 32
+    filters_rate: float = 1.5
+    max_stride: int = None
+    stem_stride: int = None
+    middle_block: bool = True
+    up_interpolate: bool = True
+    stacks: int = 3
+    convs_per_block: int = 2
 
-        Attributes:
-            model_type: (str) One of the ConvNext architecture types: ["tiny", "small", "base"]. Default: "tiny".
-            arch: Dictionary of embed dimension, depths and number of heads in each layer. Default is "Tiny architecture". {'embed': 96, 'depths': [2,2,6,2], 'channels':[3, 6, 12, 24]}
-            patch_size: (List[int]) Patch size for the stem layer of SwinT. Default: [4,4].
-            stem_patch_stride: (int) Stride for the patch. Default is 2.
-            window_size: (List[int]) Window size. Default: [7,7].
-            in_channels: (int) Number of input channels. Default is 1.
-            kernel_size: (int) Size of the convolutional kernels. Default is 3.
-            filters_rate: (float) Factor to adjust the number of filters per block. Default is 1.5.
-            convs_per_block: (int) Number of convolutional layers per block. Default is 2.
-            up_interpolate: (bool) If True, use bilinear interpolation instead of transposed convolutions for upsampling. Interpolation is faster but transposed convolutions may be able to learn richer or more complex upsampling to recover details from higher scales. Default: True.
-        """
 
-        model_type: str = "tiny"  # Options: tiny, small, base
-        arch: dict = attrs.field(
-            factory=lambda: {
-                "embed": 96,
-                "depths": [2, 2, 6, 2],
-                "channels": [3, 6, 12, 24],
-            }
-        )
-        patch_size: list = attrs.field(factory=lambda: [4, 4])
-        stem_patch_stride: int = 2
-        window_size: list = attrs.field(factory=lambda: [7, 7])
-        in_channels: int = 1
-        kernel_size: int = 3
-        filters_rate: float = 1.5
-        convs_per_block: int = 2
-        up_interpolate: bool = True
+@attrs.define
+class ConvNextConfig:
+    """convnext configuration for backbone
+
+    Attributes:
+        arch: (Default is Tiny architecture config. No need to provide if model_type is provided)
+            depths: (List(int)) Number of layers in each block. Default: [3, 3, 9, 3].
+            channels: (List(int)) Number of channels in each block. Default: [96, 192, 384, 768].
+        model_type: (str) One of the ConvNext architecture types: ["tiny", "small", "base", "large"]. Default: "tiny".
+        stem_patch_kernel: (int) Size of the convolutional kernels in the stem layer. Default is 4.
+        stem_patch_stride: (int) Convolutional stride in the stem layer. Default is 2.
+        in_channels: (int) Number of input channels. Default is 1.
+        kernel_size: (int) Size of the convolutional kernels. Default is 3.
+        filters_rate: (float) Factor to adjust the number of filters per block. Default is 1.5.
+        convs_per_block: (int) Number of convolutional layers per block. Default is 2.
+        up_interpolate: (bool) If True, use bilinear interpolation instead of transposed convolutions for upsampling. Interpolation is faster but transposed convolutions may be able to learn richer or more complex upsampling to recover details from higher scales. Default: True.
+    """
+
+    model_type: str = "tiny"  # Options: tiny, small, base, large
+    arch: dict = attrs.field(
+        factory=lambda: {"depths": [3, 3, 9, 3], "channels": [96, 192, 384, 768]}
+    )
+    stem_patch_kernel: int = 4
+    stem_patch_stride: int = 2
+    in_channels: int = 1
+    kernel_size: int = 3
+    filters_rate: float = 1.5
+    convs_per_block: int = 2
+    up_interpolate: bool = True
+
+
+@attrs.define
+class SwinTConfig:
+    """swinT configuration for backbone
+
+    Attributes:
+        model_type: (str) One of the ConvNext architecture types: ["tiny", "small", "base"]. Default: "tiny".
+        arch: Dictionary of embed dimension, depths and number of heads in each layer. Default is "Tiny architecture". {'embed': 96, 'depths': [2,2,6,2], 'channels':[3, 6, 12, 24]}
+        patch_size: (List[int]) Patch size for the stem layer of SwinT. Default: [4,4].
+        stem_patch_stride: (int) Stride for the patch. Default is 2.
+        window_size: (List[int]) Window size. Default: [7,7].
+        in_channels: (int) Number of input channels. Default is 1.
+        kernel_size: (int) Size of the convolutional kernels. Default is 3.
+        filters_rate: (float) Factor to adjust the number of filters per block. Default is 1.5.
+        convs_per_block: (int) Number of convolutional layers per block. Default is 2.
+        up_interpolate: (bool) If True, use bilinear interpolation instead of transposed convolutions for upsampling. Interpolation is faster but transposed convolutions may be able to learn richer or more complex upsampling to recover details from higher scales. Default: True.
+    """
+
+    model_type: str = "tiny"  # Options: tiny, small, base
+    arch: dict = attrs.field(
+        factory=lambda: {
+            "embed": 96,
+            "depths": [2, 2, 6, 2],
+            "channels": [3, 6, 12, 24],
+        }
+    )
+    patch_size: list = attrs.field(factory=lambda: [4, 4])
+    stem_patch_stride: int = 2
+    window_size: list = attrs.field(factory=lambda: [7, 7])
+    in_channels: int = 1
+    kernel_size: int = 3
+    filters_rate: float = 1.5
+    convs_per_block: int = 2
+    up_interpolate: bool = True
 
 
 @oneof
-@attr.s(auto_attribs=True)
+@attrs.define
 class HeadsConfig:
     """Configurations related to the model output head type.
 
