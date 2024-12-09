847bee8b93c3f00eef52069fe562f14a30138921
gitttt-1234 <divyasesh11@gmail.com>
2024-12-09 14:46:13 -0800

    Add tests

--- tests/training/test_model_trainer.py
+++ tests/training/test_model_trainer.py
@@ -685,6 +685,20 @@ def test_topdown_centered_instance_model(config, tmp_path: str):
     # check the loss value
     loss = model.training_step(input_, 0)
     assert abs(loss - mse_loss(preds, input_cm)) < 1e-3
+
+    ## check val step
+    model_trainer._create_data_loaders_litdata()
+    input_ = next(iter(model_trainer.val_data_loader))
+    input_cm = input_["confidence_maps"]
+    preds = model(input_["instance_image"])
+
+    # check the output shape
+    assert preds.shape == (1, 2, 80, 80)
+
+    # check the loss value
+    loss = model.validation_step(input_, 0)
+    assert abs(loss - mse_loss(preds, input_cm)) < 1e-3
+
     shutil.rmtree((Path(model_trainer.bin_files_path) / "train_chunks").as_posix())
     shutil.rmtree((Path(model_trainer.bin_files_path) / "val_chunks").as_posix())
 
@@ -760,6 +774,19 @@ def test_centroid_model(config, tmp_path: str):
 
     # check the loss value
     loss = model.training_step(input_, 0)
+    assert abs(loss - mse_loss(preds, input_cm.squeeze(dim=1))) < 1e-3
+
+    ## check val step
+    model_trainer._create_data_loaders_litdata()
+    input_ = next(iter(model_trainer.val_data_loader))
+    input_cm = input_["centroids_confidence_maps"]
+    preds = model(input_["image"])
+
+    # check the output shape
+    assert preds.shape == (1, 1, 192, 192)
+
+    # check the loss value
+    loss = model.validation_step(input_, 0)
     assert abs(loss - mse_loss(preds, input_cm.squeeze(dim=1))) < 1e-3
 
     shutil.rmtree((Path(model_trainer.bin_files_path) / "train_chunks").as_posix())
