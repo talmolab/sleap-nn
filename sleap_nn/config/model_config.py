cd18706d89fd7c4a6ec8721b97dec76d0277da44
gqcpm <63070177+gqcpm@users.noreply.github.com>
2025-01-06 22:22:58 -0800

    adjust default modelconfig fixture and tests

--- sleap_nn/config/model_config.py
+++ sleap_nn/config/model_config.py
@@ -1,68 +1,8 @@
 import attrs
 from enum import Enum
+from sleap_nn.config.utils import oneof
+from typing import Optional, List, Text, Any
 
-
-@attrs.define
-class ModelConfig:
-    """Configurations related to model architecture.
-
-    Attributes:
-        init_weight: (str) model weights initialization method. "default" uses kaiming uniform initialization and "xavier" uses Xavier initialization method.
-        pre_trained_weights: (str) Pretrained weights file name supported only for ConvNext and SwinT backbones. For ConvNext, one of ["ConvNeXt_Base_Weights","ConvNeXt_Tiny_Weights", "ConvNeXt_Small_Weights", "ConvNeXt_Large_Weights"]. For SwinT, one of ["Swin_T_Weights", "Swin_S_Weights", "Swin_B_Weights"].
-        backbone_config: initialize either UNetConfig, ConvNextConfig, or SwinTConfig based on input from backbone_type
-        head_config: head_configs: (Dict) Dictionary with the following keys having head configs for the model to be trained. Note: Configs should be provided only for the model to train and others should be None
-    """
-
-    init_weight: str = "default"
-    pre_trained_weights: str = None
-    backbone_config: BackboneConfig = attrs.field(factory=BackboneConfig)
-    head_configs: HeadConfig = attrs.field(factory=HeadConfig)
-
-    # post-initialization
-    def __attrs_post_init__(self):
-        self.validate_pre_trained_weights()
-
-    # validate the pre-trained weights
-    def validate_pre_trained_weights(self):
-        convnext_weights = [
-            "ConvNeXt_Base_Weights",
-            "ConvNeXt_Tiny_Weights",
-            "ConvNeXt_Small_Weights",
-            "ConvNeXt_Large_Weights",
-        ]
-        swint_weights = ["Swin_T_Weights", "Swin_S_Weights", "Swin_B_Weights"]
-
-        if self.backbone_type == BackboneType.CONVNEXT:
-            if self.pre_trained_weights not in convnext_weights:
-                raise ValueError(
-                    f"Invalid pre-trained weights for ConvNext. Must be one of {convnext_weights}"
-                )
-        elif self.backbone_type == BackboneType.SWINT:
-            if self.pre_trained_weights not in swint_weights:
-                raise ValueError(
-                    f"Invalid pre-trained weights for SwinT. Must be one of {swint_weights}"
-                )
-        elif (
-            self.backbone_type == BackboneType.UNET
-            and self.pre_trained_weights is not None
-        ):
-            raise ValueError("UNet does not support pre-trained weights.")
-
-
-@oneof
-@attrs.define
-class BackboneConfig:
-    """Configurations related to model backbone configuration.
-
-    Attributes:
-        unet: An instance of `UNetConfig`.
-        convnext: An instance of `ConvNextConfig`.
-        swint: An instance of `SwinTConfig`.
-    """
-
-    unet: Optional[UNetConfig] = None
-    convnext: Optional[ConvNextConfig] = None
-    swint: Optional[SwinTConfig] = None
 
 
 # Define configuration for each backbone type (unet, convnext, swint) configurations
@@ -161,53 +101,6 @@ class SwinTConfig:
     up_interpolate: bool = True
 
 
-@oneof
-@attrs.define
-class HeadsConfig:
-    """Configurations related to the model output head type.
-
-    Only one attribute of this class can be set, which defines the model output type.
-
-    Attributes:
-        single_instance: An instance of `SingleInstanceConfmapsHeadConfig`.
-        centroid: An instance of `CentroidsHeadConfig`.
-        centered_instance: An instance of `CenteredInstanceConfmapsHeadConfig`.
-        multi_instance: An instance of `MultiInstanceConfig`.
-        multi_class_bottomup: An instance of `MultiClassBottomUpConfig`.
-        multi_class_topdown: An instance of `MultiClassTopDownConfig`.
-    """
-
-    single_instance: Optional[SingleInstanceConfig] = None
-    centroid: Optional[CentroidConfig] = None
-    centered_instance: Optional[CenteredInstanceConfig] = None
-    bottomup: Optional[BottomUpConfig] = None
-
-
-# Head_config single instance
-@attrs.define
-class SingleInstanceConfig:
-    confmaps: Optional[SingleInstanceConfMapsConfig] = None
-
-
-# Head_config centroid
-@attrs.define
-class CentroidConfig:
-    confmaps: Optional[CentroidConfMapsConfig] = None
-
-
-# Head_config centered_instance
-@attrs.define
-class CenteredInstanceConfig:
-    confmaps: Optional[CenteredInstanceConfMapsConfig] = None
-
-
-# Head_config bottomup
-@attrs.define
-class BottomUpConfig:
-    confmaps: Optional[BottomUpConfMapsConfig] = None
-    pafs: Optional[PAFConfig] = None
-
-
 @attrs.define
 class SingleInstanceConfMapsConfig:
     """Single Instance configuration map
@@ -287,3 +180,97 @@ class PAFConfig:
     sigma: Optional[float] = None
     output_stride: Optional[float] = None
     loss_weight: Optional[float] = None
+
+
+# Head_config single instance
+@attrs.define
+class SingleInstanceConfig:
+    confmaps: Optional[SingleInstanceConfMapsConfig] = None
+
+
+# Head_config centroid
+@attrs.define
+class CentroidConfig:
+    confmaps: Optional[CentroidConfMapsConfig] = None
+
+
+# Head_config centered_instance
+@attrs.define
+class CenteredInstanceConfig:
+    confmaps: Optional[CenteredInstanceConfMapsConfig] = None
+
+
+# Head_config bottomup
+@attrs.define
+class BottomUpConfig:
+    confmaps: Optional[BottomUpConfMapsConfig] = None
+    pafs: Optional[PAFConfig] = None
+
+
+
+@oneof
+@attrs.define
+class HeadConfig:
+    """Configurations related to the model output head type.
+
+    Only one attribute of this class can be set, which defines the model output type.
+
+    Attributes:
+        single_instance: An instance of `SingleInstanceConfmapsHeadConfig`.
+        centroid: An instance of `CentroidsHeadConfig`.
+        centered_instance: An instance of `CenteredInstanceConfmapsHeadConfig`.
+        multi_instance: An instance of `MultiInstanceConfig`.
+        multi_class_bottomup: An instance of `MultiClassBottomUpConfig`.
+        multi_class_topdown: An instance of `MultiClassTopDownConfig`.
+    """
+
+    single_instance: Optional[SingleInstanceConfig] = None
+    centroid: Optional[CentroidConfig] = None
+    centered_instance: Optional[CenteredInstanceConfig] = None
+    bottomup: Optional[BottomUpConfig] = None
+
+@oneof
+@attrs.define
+class BackboneConfig:
+    """Configurations related to model backbone configuration.
+
+    Attributes:
+        unet: An instance of `UNetConfig`.
+        convnext: An instance of `ConvNextConfig`.
+        swint: An instance of `SwinTConfig`.
+    """
+
+    unet: Optional[UNetConfig] = None
+    convnext: Optional[ConvNextConfig] = None
+    swint: Optional[SwinTConfig] = None
+
+
+@attrs.define
+class ModelConfig:
+    """Configurations related to model architecture.
+
+    Attributes:
+        init_weight: (str) model weights initialization method. "default" uses kaiming uniform initialization and "xavier" uses Xavier initialization method.
+        pre_trained_weights: (str) Pretrained weights file name supported only for ConvNext and SwinT backbones. For ConvNext, one of ["ConvNeXt_Base_Weights","ConvNeXt_Tiny_Weights", "ConvNeXt_Small_Weights", "ConvNeXt_Large_Weights"]. For SwinT, one of ["Swin_T_Weights", "Swin_S_Weights", "Swin_B_Weights"].
+        backbone_config: initialize either UNetConfig, ConvNextConfig, or SwinTConfig based on input from backbone_type
+        head_config: head_configs: (Dict) Dictionary with the following keys having head configs for the model to be trained. Note: Configs should be provided only for the model to train and others should be None
+    """
+
+    init_weight: str = "default"
+    pre_trained_weights: str = None
+    backbone_config: BackboneConfig = attrs.field(factory=BackboneConfig)
+    head_configs: HeadConfig = attrs.field(factory=HeadConfig)
+
+    # post-initialization
+    def __attrs_post_init__(self):
+        self.validate_pre_trained_weights()
+
+    # validate the pre-trained weights
+    def validate_pre_trained_weights(self):
+        convnext_weights = [
+            "ConvNeXt_Base_Weights",
+            "ConvNeXt_Tiny_Weights",
+            "ConvNeXt_Small_Weights",
+            "ConvNeXt_Large_Weights",
+        ]
+        swint_weights = ["Swin_T_Weights", "Swin_S_Weights", "Swin_B_Weights"]
--- tests/config/test_model_config.py
+++ tests/config/test_model_config.py
@@ -17,6 +17,61 @@ from sleap_nn.config.model_config import (
     PAFConfig
 )
 
+@pytest.fixture
 def default_config():
     """Fixture for a default ModelConfig instance."""
-    return ModelConfig(model_type="default", input_size=(224, 224), num_classes=10)
+    return ModelConfig(
+                        init_weight="default", 
+                        pre_trained_weights=None,
+                        backbone_config=BackboneConfig(),
+                        head_configs=HeadConfig()
+                        )
+
+
+def test_default_initialization(default_config):
+    """Test default initialization of ModelConfig."""
+    assert default_config.init_weight == "default"
+    assert default_config.pre_trained_weights == None
+
+# def test_invalid_pre_trained_weights():
+#     """Test validation failure with an invalid pre_trained_weights."""
+#     with pytest.raises(ValueError):
+#         ModelConfig(pre_trained_weights="weights")
+
+# def test_invalid_input_size():
+#     """Test validation failure with an invalid input_size."""
+#     with pytest.raises(ValueError, match="input_size must be a tuple of two positive integers"):
+#         ModelConfig(model_type="default", input_size=(224, -1), num_classes=10)
+
+#     with pytest.raises(ValueError, match="input_size must be a tuple of two positive integers"):
+#         ModelConfig(model_type="default", input_size="224,224", num_classes=10)
+
+# def test_invalid_num_classes():
+#     """Test validation failure with an invalid num_classes."""
+#     with pytest.raises(ValueError, match="num_classes must be a positive integer"):
+#         ModelConfig(model_type="default", input_size=(224, 224), num_classes=-5)
+
+# def test_update_config(default_config):
+#     """Test updating configuration attributes."""
+#     default_config.model_type = "resnet"
+#     default_config.input_size = (256, 256)
+#     default_config.num_classes = 20
+
+#     assert default_config.model_type == "resnet"
+#     assert default_config.input_size == (256, 256)
+#     assert default_config.num_classes == 20
+
+# def test_validation_on_update(default_config):
+#     """Test validation logic when updating attributes."""
+#     with pytest.raises(ValueError, match="num_classes must be a positive integer"):
+#         default_config.num_classes = 0
+
+#     with pytest.raises(ValueError, match="input_size must be a tuple of two positive integers"):
+#         default_config.input_size = (-1, 224)
+
+# def test_repr(default_config):
+#     """Test the string representation of ModelConfig."""
+#     expected_repr = (
+#         "ModelConfig(model_type='default', input_size=(224, 224), num_classes=10)"
+#     )
+#     assert repr(default_config) == expected_repr
