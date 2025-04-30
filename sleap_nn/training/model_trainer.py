c8aac9d27b6436a1d0afd686cdc0767bc42de637
gitttt-1234 <divyasesh@Divyas-MacBook-Air.local>
2025-04-30 11:14:42 -0700

    Explicitly close video handles

--- sleap_nn/training/model_trainer.py
+++ sleap_nn/training/model_trainer.py
@@ -1367,6 +1367,16 @@ class MultiHeadModelTrainer:
             else True
         )
 
+        # If using caching, close the videos to prevent `h5py objects can't be pickled error` when num_workers > 0.
+        if "cache_img" in self.data_pipeline_fw:
+            for video in self.train_labels[d_num].videos:
+                if video.is_open:
+                    video.close()
+
+            for video in self.val_labels[d_num].videos:
+                if video.is_open:
+                    video.close()
+
         if self.config.trainer_config.trainer_devices > 1:
             # add sampler if using more than 1 gpu
             train_sampler = DistributedSampler(
