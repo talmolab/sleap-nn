fbe365b8bda9904cc3ae9e1a82f153076d0a6cea
Tom Han <103209542+tom21100227@users.noreply.github.com>
2025-08-20 10:31:31 -0700

    lint train

--- sleap_nn/train.py
+++ sleap_nn/train.py
@@ -445,12 +445,14 @@ def train(
     run_training(omegaconf_config.copy())
 
 
-@hydra.main(version_base=None, config_path='config', config_name="training")
+@hydra.main(version_base=None, config_path="config", config_name="training")
 def main(cfg: DictConfig):
     """Train SLEAP-NN model using CLI."""
     if not cfg.trainer_config:
         # set save_ckpt_path to current working dir if not specified
-        print("No trainer config found! Use `sleap-nn-train --help` for more information.")
+        print(
+            "No trainer config found! Use `sleap-nn-train --help` for more information."
+        )
         raise SystemExit(2)
     logger.info("Input config:")
     logger.info("\n" + OmegaConf.to_yaml(cfg))
