f4e4932bc549c184b1dfbca3d5a2d83bf6e285a2
7174Andy <hep003@ucsd.edu>
2025-08-21 10:41:30 -0700

    bug fix for swint backbone model

--- sleap_nn/training/utils.py
+++ sleap_nn/training/utils.py
@@ -22,8 +22,10 @@ def get_dist_rank():
 def xavier_init_weights(x):
     """Function to initilaise the model weights with Xavier initialization method."""
     if isinstance(x, nn.Conv2d) or isinstance(x, nn.Linear):
-        nn.init.xavier_uniform_(x.weight)
-        nn.init.constant_(x.bias, 0)
+        if x.weight is not None:
+            nn.init.xavier_uniform_(x.weight)
+        if x.bias is not None:
+            nn.init.constant_(x.bias, 0)
 
 
 def imgfig(
