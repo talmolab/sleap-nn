2c9d46f01feb45b3d517aaeabcf7d41dac712b0b
gitttt-1234 <divyasesh11@gmail.com>
2025-09-18 16:30:50 -0700

    Fix sorting

--- sleap_nn/training/model_trainer.py
+++ sleap_nn/training/model_trainer.py
@@ -895,9 +895,11 @@ class ModelTrainer:
             and self.config.trainer_config.trainer_devices < torch.cuda.device_count()
             and self.config.trainer_config.trainer_device_indices is None
         ):
-            devices = np.argsort(get_gpu_memory())[
-                : self.config.trainer_config.trainer_devices
-            ]
+            devices = list(
+                np.argsort(get_gpu_memory())[::-1][
+                    : self.config.trainer_config.trainer_devices
+                ]
+            )
 
         # create lightning.Trainer instance.
         self.trainer = L.Trainer(
